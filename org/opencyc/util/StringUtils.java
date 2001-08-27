339486d6b01b3d84d27ca297509437e0996e49a1
stephenreed <>
2001-08-27 23:22:59 +0000

    initial add

---
+++ org/opencyc/util/StringUtils.java
@@ -0,0 +1,54 @@
+package org.opencyc.util;
+import ViolinStrings.*;
+
+/**
+ * Provides <tt>String</tt> utilities not otherwise provided by Violin Strings.  All methods
+ * are static.  There is no need to instantiate this class.<p>
+ *
+ * @version $Id$
+ * @author Stephen L. Reed
+ *
+ * <p>Copyright 2001 Cycorp, Inc., license is open source GNU LGPL.
+ * <p><a href="http://www.opencyc.org/license.txt">the license</a>
+ * <p><a href="http://www.opencyc.org">www.opencyc.org</a>
+ * <p><a href="http://www.sourceforge.net/projects/opencyc">OpenCyc at SourceForge</a>
+ * <p>
+ * THIS SOFTWARE AND KNOWLEDGE BASE CONTENT ARE PROVIDED ``AS IS'' AND
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OPENCYC
+ * ORGANIZATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE AND KNOWLEDGE
+ * BASE CONTENT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+public class StringUtils {
+
+    /**
+     * Returns true iff all characters in the given string are numeric.  A prefix character of
+     * "+" or "-" is accepted as numeric.  Decimal points are not accepted as numeric by this
+     * method.
+     *
+     * @param string the string to be tested
+     * @return <tt>true</tt> iff all characters are numeric
+     */
+    public static boolean isNumeric (String string) {
+        String testString = string;
+        if ((string.charAt(0) == '+') ||
+            (string.charAt(0) == '-')) {
+            if (string.length() > 1)
+                testString = string.substring(1);
+            else
+                return false;
+        }
+        for (int i = 0; i < testString.length(); i++) {
+            if (! Character.isDigit(testString.charAt(i)))
+                return false;
+        }
+        return true;
+    }
+}

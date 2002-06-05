9b98daecdc9eca2d2471b99337f255abd68acf6f
stephenreed <>
2002-06-05 15:38:04 +0000

    Initial check-in

---
+++ org/opencyc/cycobject/CycObject.java
@@ -0,0 +1,21 @@
+/*
+ * CycObject.java
+ *
+ * Created on May 10, 2002, 11:35 AM
+ */
+
+package org.opencyc.cycobject;
+
+/**
+ *
+ * @author  tbrussea
+ */
+public class CycObject {
+  
+  /** Creates a new instance of CycObject */
+  public CycObject() {
+  }
+    
+  public String cyclify() { return toString(); }
+  
+}

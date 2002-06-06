f374155a68d98cb11a7526ec9524d9c982c4fa53
dmiles <>
2002-06-06 08:54:20 +0000

    Updated install script to bail if $SWI_HOME is not set.

--- swi_interface/install.sh
+++ swi_interface/install.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
 
-cat */interface.pl utilities.pl > opencyc.pl
+if [ -z "$SWI_HOME" ] ; then
+  echo SWI_HOME is not set. try
+  echo export SWI_HOME=/usr/lib/pl-5.0.6
+  echo exiting..
+  exit 1;
+fi
+
+cd `dirname $0`
+pwd
+cat */interface.pl */utilities.pl > opencyc.pl
 
 cat */e2c.pl > e2c.pl
 
 cp *.pl  $SWI_HOME/library
 
-pl -g "[opencyc],testOpenCyc."
+pl -g "ensure_loaded(library(opencyc)),testOpenCyc."
 
-pl -g "[e2c],testE2C."
+pl -g "ensure_loaded(library(e2c)),testE2C."
+
+cd -
+
 
 
 

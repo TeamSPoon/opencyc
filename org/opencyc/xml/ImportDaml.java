46c6d093164f4d000129b16cb9f4d7960c0a02ec
stephenreed <>
2002-09-10 03:31:39 +0000

    Added code for DAML import.

--- org/opencyc/xml/ImportDaml.java
+++ org/opencyc/xml/ImportDaml.java
@@ -52,6 +52,17 @@ public class ImportDaml {
     protected int verbosity = DEFAULT_VERBOSITY;
 
     /**
+     * The list of DAML web documents to import.
+     */
+    public static ArrayList documentsToImport = new ArrayList();
+
+    /**
+     * Ontology library nicknames, which become namespace identifiers
+     * upon import into Cyc.
+     */
+    protected HashMap ontologyNicknames = new HashMap();
+
+    /**
      * Constructs a new ImportDaml object.
      */
     public ImportDaml() {
@@ -60,22 +71,74 @@ public class ImportDaml {
     /**
      * Provides the main method for the ImportDaml application.
      *
-     * @param args optionally provide the path to the DAML document.
+     * @param args ignored.
      */
-    public static void main(String[] argv) {
+    public static void main(String[] args) {
         Log.makeLog();
-        String damlPath = "file:///opencyc/xml/military-elements-ont-1.daml";
-        //if (args.length > 0)
-        //   umlModelPath = args[0];
-        ImportDaml importDaml = new ImportDaml();
-        importDaml.initialize();
-        importDaml.importDaml(damlPath);
+        initializeDocumentsToImport();
+        //for (int i = 0; i < documentsToImport.size(); i++) {
+        for (int i = 0; i < 2; i++) {
+            String damlPath = (String) documentsToImport.get(i);
+            ImportDaml importDaml = new ImportDaml();
+            importDaml.initialize();
+            importDaml.importDaml(damlPath);
+        }
+    }
+
+    /**
+     * Initializes the documents to import.
+     */
+    protected static void initializeDocumentsToImport () {
+        documentsToImport.add("http://www.daml.org/experiment/ontology/agency-ont.daml");
+        documentsToImport.add("http://www.daml.org/experiment/ontology/CINC-ont.daml");
+        documentsToImport.add("http://www.daml.org/experiment/ontology/af-a.daml");
+        documentsToImport.add("http://www.daml.org/experiment/ontology/assessment-ont.daml");
+        documentsToImport.add("http://www.daml.org/experiment/ontology/economic-elements-ont.daml");
+        documentsToImport.add("http://www.daml.org/experiment/ontology/elements-ont.daml");
+        documentsToImport.add("http://www.daml.org/experiment/ontology/information-elements-ont.daml");
+        documentsToImport.add("http://www.daml.org/experiment/ontology/infrastructure-elements-ont.daml");
+        documentsToImport.add("http://www.daml.org/experiment/ontology/location-ont.daml");
+        documentsToImport.add("http://www.daml.org/experiment/ontology/military-elements-ont.daml");
+        documentsToImport.add("http://www.daml.org/experiment/ontology/objectives-ont.daml");
+        documentsToImport.add("http://www.daml.org/experiment/ontology/operation-ont.daml");
+        documentsToImport.add("http://www.daml.org/experiment/ontology/political-elements-ont.daml");
+        documentsToImport.add("http://www.daml.org/experiment/ontology/social-elements-ont.daml");
+        documentsToImport.add("http://www.daml.org/experiment/ontology/example1.daml");
+        documentsToImport.add("http://www.daml.org/experiment/ontology/example2.daml");
     }
 
     /**
      * Initializes the ImportDaml object.
      */
     protected void initialize () {
+        initializeOntologyNicknames();
+    }
+
+    /**
+     * Initializes the ImportDaml object.
+     */
+    protected void initializeOntologyNicknames () {
+        ontologyNicknames.put("http://www.w3.org/1999/02/22-rdf-syntax-ns", "rdf");
+        ontologyNicknames.put("http://www.w3.org/2000/01/rdf-schema", "rdfs");
+        ontologyNicknames.put("http://www.w3.org/2000/10/XMLSchema", "xsd");
+        ontologyNicknames.put("http://www.daml.org/experiment/ontology/elements-ont", "NS0");
+        ontologyNicknames.put("http://www.daml.org/2001/09/countries/fips", "fips");
+        ontologyNicknames.put("http://www.daml.org/2001/10/html/airport-ont", "airport");
+        ontologyNicknames.put("http://orlando.drc.com/daml/ontology/VES/3.2/drc-ves-ont", "ves");
+        ontologyNicknames.put("http://www.daml.org/experiment/ontology/elements-ont", "enp");
+        ontologyNicknames.put("http://www.daml.org/experiment/ontology/objectives-ont", "obj");
+        ontologyNicknames.put("http://www.daml.org/experiment/ontology/social-elements-ont", "soci");
+        ontologyNicknames.put("http://www.daml.org/experiment/ontology/political-elements-ont", "poli");
+        ontologyNicknames.put("http://www.daml.org/experiment/ontology/economic-elements-ont", "econ");
+        ontologyNicknames.put("http://www.daml.org/experiment/ontology/infrastructure-elements-ont", "infr");
+        ontologyNicknames.put("http://www.daml.org/experiment/ontology/information-elements-ont", "info");
+        ontologyNicknames.put("http://www.daml.org/experiment/ontology/military-elements-ont", "mil");
+        ontologyNicknames.put("http://www.daml.org/experiment/ontology/ona.xsd", "dt");
+        ontologyNicknames.put("http://www.daml.org/experiment/ontology/location-ont", "loc");
+        ontologyNicknames.put("http://www.daml.org/experiment/ontology/assessment-ont", "assess");
+        ontologyNicknames.put("http://www.daml.org/2001/02/geofile/geofile-dt.xsd", "geodt");
+        ontologyNicknames.put("http://www.daml.org/experiment/ontology/cinc-ont", "cinc");
+        ontologyNicknames.put("http://www.daml.org/experiment/ontology/agency-ont", "agent");
     }
 
     /**
@@ -83,7 +146,8 @@ public class ImportDaml {
      */
     protected void importDaml (String damlPath) {
         DAMLModel damlModel = new DAMLModelImpl();
-        System.out.println("Loading " + damlPath);
+        if (verbosity > 0)
+            System.out.println("\nImporting " + damlPath);
         try {
             damlModel.read(damlPath);
         }
@@ -91,7 +155,7 @@ public class ImportDaml {
             e.printStackTrace();
         }
         System.out.println(damlModel.toString());
-        System.out.println("\nProperties\n");
+        System.out.println("Properties");
 
         Iterator iter = damlModel.listDAMLProperties();
         while (iter.hasNext()) {
@@ -100,16 +164,29 @@ public class ImportDaml {
         }
 
 
-        System.out.println("\nClasses and Properties\n");
+        System.out.println("Classes\n");
         iter = damlModel.listDAMLClasses();
         while (iter.hasNext()) {
             DAMLClass damlClass = (DAMLClass)iter.next();
             System.out.println(damlClass.toString());
-            Iterator iterProperties = damlClass.getDefinedProperties();
-            while (iterProperties.hasNext()) {
-                System.out.println("    "+iterProperties.next().toString());
+            Literal literal = damlClass.prop_label().getValue();
+            if (literal != null)
+                System.out.println("  label " + literal.toString());
+            LiteralAccessor comment = damlClass.prop_comment();
+            if (literal != null)
+                System.out.println("  comment " + comment.toString());
+            Iterator iterSuperClasses = damlClass.getSuperClasses(false);
+            while (iterSuperClasses.hasNext()) {
+                System.out.println("  super class " + iterSuperClasses.next().toString());
             }
+            Iterator iterInstances = damlClass.getInstances();
+            while (iterInstances.hasNext()) {
+                System.out.println("  instances " + iterInstances.next().toString());
+            }
+
         }
+        if (verbosity > 0)
+            System.out.println("\nDone importing " + damlPath + "\n");
     }
 
     /**

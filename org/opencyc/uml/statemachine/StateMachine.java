20020986ab080b83e26cab1a0d88db84cbc525e9
stephenreed <>
2002-10-25 23:54:58 +0000

    Adding classes for UML state machines

---
+++ org/opencyc/uml/statemachine/StateMachine.java
@@ -0,0 +1,61 @@
+package org.opencyc.uml.statemachine;
+
+import java.util.*;
+import org.opencyc.uml.core.*;
+
+
+/**
+ * StateMachine from the UML State_Machines package.
+ *
+ * @version $Id$
+ * @author Stephen L. Reed
+ *
+ * <p>Copyright 2001 Cycorp, Inc., license is open source GNU LGPL.
+ * <p><a href="http://www.opencyc.org/license.txt">the license</a>
+ * <p><a href="http://www.opencyc.org">www.opencyc.org</a>
+ * <p><a href="http://www.sourceforge.net/projects/opencyc">OpenCyc at SourceForge</a>
+ * <p>
+ * THIS SOFTWARE AND KNOWLEDGE BASE CONTENT ARE PROVIDED ``AS IS'' AND
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OPENCYC
+ * ORGANIZATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE AND KNOWLEDGE
+ * BASE CONTENT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+public class StateMachine extends ModelElement {
+
+    /**
+     * context of this state machine
+     */
+    public ModelElement context;
+
+    /**
+     * top state of this state machine
+     */
+    public State top;
+
+    /**
+     * Transitions for this state machine
+     */
+    public ArrayList transitions = new ArrayList();
+
+    /**
+     * submachine state for this state machine
+     */
+    public SubMachineState submachineState;
+
+
+
+    /**
+     * Constructs a new StateMachine object.
+     */
+    public StateMachine() {
+    }
+}
---
+++ org/opencyc/uml/statemachine/StateVertex.java
@@ -0,0 +1,55 @@
+package org.opencyc.uml.statemachine;
+
+import java.util.*;
+import org.opencyc.uml.core.*;
+
+
+/**
+ * StateVertex from the UML State_Machines package.
+ *
+ * @version $Id$
+ * @author Stephen L. Reed
+ *
+ * <p>Copyright 2001 Cycorp, Inc., license is open source GNU LGPL.
+ * <p><a href="http://www.opencyc.org/license.txt">the license</a>
+ * <p><a href="http://www.opencyc.org">www.opencyc.org</a>
+ * <p><a href="http://www.sourceforge.net/projects/opencyc">OpenCyc at SourceForge</a>
+ * <p>
+ * THIS SOFTWARE AND KNOWLEDGE BASE CONTENT ARE PROVIDED ``AS IS'' AND
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OPENCYC
+ * ORGANIZATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE AND KNOWLEDGE
+ * BASE CONTENT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+public class StateVertex extends ModelElement {
+
+    /**
+     * the container of this state vertex
+     */
+    public CompositeState container;
+
+    /**
+     * outgoing transitions from this state vertex
+     */
+    public ArrayList outgoing = new ArrayList();
+
+    /**
+     * incoming transitions into this state vertex
+     */
+    public ArrayList incoming = new ArrayList();
+
+
+    /**
+     * Constructs a new StateVertex object.
+     */
+    public StateVertex() {
+    }
+}

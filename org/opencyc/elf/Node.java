d83aea675050210cf9b5bf88515d9486105c0c15
stephenreed <>
2002-11-11 04:58:11 +0000

    Added new classes to support Experience per Albus/NIST reference model.

--- org/opencyc/elf/Node.java
+++ org/opencyc/elf/Node.java
@@ -6,7 +6,6 @@ import java.util.*;
 /**
  * Provides the Node container for the Elementary Loop Functioning (ELF).<br>
  *
- *
  * @version $Id$
  * @author Stephen L. Reed
  *
@@ -33,59 +32,59 @@ public class Node {
 
 
     /**
-     * Dictionary of nodes by name.
+     * the dictionary of nodes by name
      * name --> Node
      */
-    static HashMap nodes = new HashMap();
+    protected static HashMap nodes = new HashMap();
 
     /**
-     * unique name of this node
+     * the unique name of this node
      */
     protected String name;
 
     /**
-     * purpose description
+     * the purpose description
      */
     protected String purpose;
 
     /**
-     * World Model for this node
+     * the World Model for this node
      */
     protected WorldModel worldModel;
 
     /**
-     * ValueJudgement for this node
+     * the ValueJudgement for this node
      */
     protected ValueJudgement valueJudgement;
 
     /**
-     * BehaviorGeneration for this node
+     * the BehaviorGeneration for this node
      */
     protected BehaviorGeneration behaviorGeneration;
 
     /**
-     * Actuator for this node
+     * the Actuator for this node
      */
     protected Actuator actuator;
 
     /**
-     * Sensor for this node
+     * the Sensor for this node
      */
     protected Sensor sensor;
 
     /**
-     * SensoryPerception
+     * the SensoryPerception
      */
     protected SensoryPerception sensoryPerception;
 
     /**
-     * Parent node of this node for the task at hand.  The topmost
+     * The parent node of this node for the task at hand.  The topmost
      * node has value null for parentNode.
      */
     protected Node parentNode;
 
     /**
-     * Child nodes of this node for the task at hand.  The lowest
+     * The Child nodes of this node for the task at hand.  The lowest
      * level nodes in the hierarchy have null for childNodes;
      */
     protected ArrayList childNodes;
@@ -109,5 +108,203 @@ public class Node {
         stringBuffer.append("  purpose: ");
         stringBuffer.append(purpose);
         return stringBuffer.toString();
+    }
+
+    /**
+     * Gets the dictionary of nodes by name
+     *
+     * @return the dictionary of nodes by name
+     */
+    public HashMap getNodes () {
+        return nodes;
+    }
+
+    /**
+     * Sets the dictionary of nodes by name
+     *
+     * @param nodes the dictionary of nodes by name
+     */
+    public void setNodes (HashMap nodes) {
+        this.nodes = nodes;
+    }
+
+    /**
+     * Gets the unique name of this node
+     *
+     * @return the unique name of this node
+     */
+    public String getName () {
+        return name;
+    }
+
+    /**
+     * Sets the unique name of this node
+     *
+     * @param name the unique name of this node
+     */
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets the purpose description
+     *
+     * @return the purpose description
+     */
+    public String getPurpose () {
+        return purpose;
+    }
+
+    /**
+     * Sets the purpose description
+     *
+     * @param purpose the purpose description
+     */
+    public void setPurpose (String purpose) {
+        this.purpose = purpose;
+    }
+
+    /**
+     * Gets the World Model for this node
+     *
+     * @return the World Model for this node
+     */
+    public WorldModel getWorldModel () {
+        return worldModel;
+    }
+
+    /**
+     * Sets the World Model for this node
+     *
+     * @param worldModel the World Model for this node
+     */
+    public void setWorldModel (WorldModel worldModel) {
+        this.worldModel = worldModel;
+    }
+
+    /**
+     * Gets the ValueJudgement for this node
+     *
+     * @return the ValueJudgement for this node
+     */
+    public ValueJudgement getValueJudgement () {
+        return valueJudgement;
+    }
+
+    /**
+     * Sets the ValueJudgement for this node
+     *
+     * @param valueJudgement the ValueJudgement for this node
+     */
+    public void setValueJudgement (ValueJudgement valueJudgement) {
+        this.valueJudgement = valueJudgement;
+    }
+
+    /**
+     * Gets the BehaviorGeneration for this node
+     *
+     * @return the BehaviorGeneration for this node
+     */
+    public BehaviorGeneration getBehaviorGeneration () {
+        return behaviorGeneration;
+    }
+
+    /**
+     * Sets the BehaviorGeneration for this node
+     *
+     * @param behaviorGeneration the BehaviorGeneration for this node
+     */
+    public void setBehaviorGeneration (BehaviorGeneration behaviorGeneration) {
+        this.behaviorGeneration = behaviorGeneration;
+    }
+
+    /**
+     * Gets the Actuator for this node
+     *
+     * @return the Actuator for this node
+     */
+    public Actuator getActuator () {
+        return actuator;
+    }
+
+    /**
+     * Sets the Actuator for this node
+     *
+     * @param actuator the Actuator for this node
+     */
+    public void setActuator (Actuator actuator) {
+        this.actuator = actuator;
+    }
+
+    /**
+     * Gets the Sensor for this node
+     *
+     * @return the Sensor for this node
+     */
+    public Sensor getSensor () {
+        return sensor;
+    }
+
+    /**
+     * Sets the Sensor for this node
+     *
+     * @param sensor the Sensor for this node
+     */
+    public void setSensor (Sensor sensor) {
+        this.sensor = sensor;
+    }
+
+    /**
+     * Gets the SensoryPerception
+     *
+     * @return the SensoryPerception
+     */
+    public SensoryPerception getSensoryPerception () {
+        return sensoryPerception;
+    }
+
+    /**
+     * Sets the SensoryPerception
+     *
+     * @param sensoryPerception the SensoryPerception
+     */
+    public void setSensoryPerception (SensoryPerception sensoryPerception) {
+        this.sensoryPerception = sensoryPerception;
+    }
+
+    /**
+     * Gets the parent node of this node for the task at hand
+     *
+     * @return the parent node of this node for the task at hand
+     */
+    public Node getParentNode () {
+        return parentNode;
+    }
+
+    /**
+     * Sets the parent node of this node for the task at hand
+     *
+     * @param parentNode the parent node of this node for the task at hand
+     */
+    public void setParentNode (Node parentNode) {
+        this.parentNode = parentNode;
+    }
+
+    /**
+     * Gets the Child nodes of this node for the task at hand
+     *
+     * @return the Child nodes of this node for the task at hand
+     */
+    public ArrayList getChildNodes () {
+        return childNodes;
+    }
+
+    /**
+     * Sets the Child nodes of this node for the task at hand
+     *
+     * @param childNodes the Child nodes of this node for the task at hand
+     */
+    public void setChildNodes (ArrayList childNodes) {
+        this.childNodes = childNodes;
     }
 }

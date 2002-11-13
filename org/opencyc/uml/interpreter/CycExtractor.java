dc09ae9909a8f3b13c9aac3a5b6d25a099cb4f35
stephenreed <>
2002-11-13 00:49:57 +0000

    Initial check in

---
+++ org/opencyc/uml/interpreter/CycExtractor.java
@@ -0,0 +1,102 @@
+package org.opencyc.uml.interpreter;
+
+import java.io.*;
+import java.util.*;
+import org.opencyc.api.*;
+import org.opencyc.cycobject.*;
+import org.opencyc.uml.statemachine.*;
+
+/**
+ * Extracts a state machine model from the Cyc KB.
+ *
+ * @version $Id$
+ * @author Stephen L. Reed
+ *
+ * <p>Copyright 2001 Cycorp, Inc., license is open source GNU LGPL.
+ * <p><a href="http://www.opencyc.org/license.txt">the license</a>
+ * <p><a href="http://www.opencyc.org">www.opencyc.org</a>
+ * <p><a href="http://www.sourceforge.net/projects/opencyc">OpenCyc at SourceForge</a>
+ * <p>
+ * THIS SOFTWARE AND KNOWLEDGE BASE CONTENT ARE PROVIDED ``AS IS'' AND
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OPENCYC
+ * ORGANIZATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE AND KNOWLEDGE
+ * BASE CONTENT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+public class CycExtractor {
+
+    /**
+     * the CycAccess object which manages the Cyc server connection
+     */
+    protected CycAccess cycAccess;
+
+    /**
+     * the state machine factory
+     */
+    protected StateMachineFactory stateMachineFactory;
+
+    /**
+     * the relevant inference microtheory
+     */
+    protected CycFort mt;
+
+
+    /**
+     * the state machine term
+     */
+    protected CycConstant stateMachineTerm;
+
+    /**
+     * Construct a new CycExtractor object given the CycAccess
+     * server connection.
+     *
+     * @param cycAcess the given CycAccess Cyc KB server connection
+     */
+    public CycExtractor(CycAccess cycAccess) {
+        this.cycAccess = cycAccess;
+        stateMachineFactory = new StateMachineFactory();
+    }
+
+    /**
+     * Extracts the state machine model specified by the given name.
+     *
+     * @param name the name of the state machine to be extracted from Cyc
+     * @return the state machine model specified by the given name
+     */
+    public StateMachine extract (String name)
+        throws IOException, CycApiException {
+        mt = cycAccess.getKnownConstantByName("UMLStateMachineSpindleCollectorMt");
+        StateMachine stateMachine = extractStateMachine(name);
+
+        return stateMachine;
+    }
+
+    /**
+     * Extracts the state machine from Cyc.
+     *
+     * @param name the name of the state machine to be extracted from Cyc
+     */
+    protected StateMachine extractStateMachine (String name)
+            throws IOException, CycApiException {
+        stateMachineTerm = cycAccess.getConstantByName(name);
+        String namespaceName = "";
+
+        String commentString = "";
+
+        Object context = "";
+
+        return stateMachineFactory.makeStateMachine(namespaceName,
+                                                    name,
+                                                    commentString,
+                                                    context);
+    }
+}

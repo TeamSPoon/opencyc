ad6b2da650f35b14572e80bf286429bca01d97a9
yepster <>
2002-12-11 16:31:32 +0000

    Removed old 0.6 files; files for 0.7 will be in a tgz.

--- org/opencyc/oracle/loadjars.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-loadjava -f -verbose -oci8 -u cyctest/cyctest ~/opencyc-0.7.0/lib/jakarta-oro-2.0.6.jar &
-loadjava -f -verbose -oci8 -u cyctest/cyctest ~/opencyc-0.7.0/lib/violinStrings.jar &
-loadjava -f -verbose -oci8 -u cyctest/cyctest ~/opencyc-0.7.0/lib/jdom.jar &
-loadjava -f -verbose -oci8 -u cyctest/cyctest ~/opencyc-0.7.0/lib/xmlParserAPIs.jar &
-loadjava -f -verbose -oci8 -u cyctest/cyctest oracle-opencyc.jar &

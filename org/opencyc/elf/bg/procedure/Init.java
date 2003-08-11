bfb04a9654a1fb6f0afebbe5375d316d06f4f24a
stephenreed <>
2003-08-11 22:32:21 +0000

    Updated javadoc for the license terms

--- org/opencyc/elf/bg/procedure/Init.java
+++ org/opencyc/elf/bg/procedure/Init.java
@@ -1,10 +1,3 @@
-/* $Id$
- *
- * Copyright (c) 2003 Cycorp, Inc.  All rights reserved.
- * This software is the proprietary information of Cycorp, Inc.
- * Use is subject to license terms.
- */
-
 package org.opencyc.elf.bg.procedure;
 
 //// Internal Imports
@@ -16,11 +9,24 @@ import java.util.ArrayList;
 /**
  * <P>Init is designed to...
  *
- * <P>Copyright (c) 2003 Cycorp, Inc.  All rights reserved.
- * <BR>This software is the proprietary information of Cycorp, Inc.
- * <P>Use is subject to license terms.
- *
- * @author reed
+ * @author Stephen L. Reed  
+ * <p>Copyright 2001 Cycorp, Inc., license is open source GNU LGPL.
+ * <p><a href="http://www.opencyc.org/license.txt">the license</a>
+ * <p><a href="http://www.opencyc.org">www.opencyc.org</a>
+ * <p><a href="http://www.sourceforge.net/projects/opencyc">OpenCyc at SourceForge</a>
+ * <p>
+ * THIS SOFTWARE AND KNOWLEDGE BASE CONTENT ARE PROVIDED ``AS IS'' AND
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OPENCYC
+ * ORGANIZATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE AND KNOWLEDGE
+ * BASE CONTENT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * @date August 11, 2003, 4:24 PM
  * @version $Id$
  */
@@ -28,7 +34,6 @@ public class Init extends org.opencyc.elf.bg.procedure.Procedure {
   
   //// Constructors
   
-  /** Creates a new instance of Init. */
   /**
    * Creates a new instance of Init.
    * @param namespace the procedure namespace

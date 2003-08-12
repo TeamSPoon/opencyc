e7ed0e4c7cc3dfb74d1e2ef40a07fd532fd3d4eb
stephenreed <>
2003-08-12 21:24:16 +0000

    Added method signatures.

--- org/opencyc/elf/bg/BehaviorGeneration.java
+++ org/opencyc/elf/bg/BehaviorGeneration.java
@@ -1,9 +1,20 @@
 package org.opencyc.elf.bg;
 
+//// Internal Imports
+import org.opencyc.elf.NodeComponent;
+
+import org.opencyc.elf.bg.planner.Schedule;
+
+import org.opencyc.elf.bg.taskframe.TaskCommand;
+import org.opencyc.elf.bg.taskframe.TaskFrame;
+
+import org.opencyc.elf.vj.Result;
+
+//// External Imports
+import org.opencyc.elf.goal.Goal;
+
 import java.util.ArrayList;
 
-import org.opencyc.elf.NodeComponent;
-import org.opencyc.elf.goal.Goal;
 
 
 /**
@@ -31,27 +42,143 @@ import org.opencyc.elf.goal.Goal;
  * BASE CONTENT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 public class BehaviorGeneration extends NodeComponent {
-  /** the commanded goal for generated behavior to achieve */
-  protected Goal commandedGoal;
 
-  /**
-   * the parent node's BehaviorGeneration object.  The topmost
-   * BehaviorGeneration object has a value null here.
-   */
-  protected BehaviorGeneration parentBehaviorGeneration;
-
-  /**
-   * the child nodes' BehaviorGeneration objects.  The lowest level
-   * BehavoriGeneration object has a value null here.
-   */
-  protected ArrayList childrenBehaviorGeneration;
-
+  //// Constructors
+  
   /**
    * Constructs a new BehaviorGeneration object.
    */
   public BehaviorGeneration() {
   }
 
+  //// Public Area
+  
+  /**
+   * Performs the commanded task.
+   *
+   * @taskCommand the commanded task
+   */
+  public void doTask (TaskCommand taskCommand) {
+    //TODO
+  }
+  
+  /**
+   * Receives the task command and task frame.
+   * 
+   * @param taskCommand the task command
+   * @param taskFrame the task frame
+   */
+  public void receiveTaskFrame (TaskCommand taskCommand, TaskFrame taskFrame) {
+    //TODO
+  }
+  
+  /**
+   * Receives the value judgement result for the given schedule, controlled resources and task
+   * frame.
+   *
+   * @param controlledResources the given list of controlled resources
+   * @param taskFrame the given task frame
+   * @param schedule the given schedule
+   * @param result the received value judgement result
+   */
+  public void receiveScheduleEvaluation (ArrayList controlledResources,
+                                         TaskFrame taskFrame,
+                                         Schedule schedule,
+                                         Result result) {
+    //TODO
+    // receive via channel
+  }
+  
+  /**
+   * Receives a task command failure notification.
+   *
+   * @param controlledResources the given list of controlled resources
+   * @param taskCommand the given task command
+   */
+  public void failureNotification (ArrayList controlledResources,
+                                   TaskCommand taskCommand) {
+    //TODO
+    // receive via channel
+  }
+  
+  /**
+   * Receives the status from value judgement with regard to the given list of controlled
+   * resources, task frame and schedule.
+   *
+   * @param controlledResources the given list of controlled resources
+   * @param taskFrame the given task frame
+   * @param schedule the given schedule
+   * @param status the received value judgement status
+   */
+  public void receiveValueJudgementStatus (ArrayList controlledResources,
+                                           TaskFrame taskFrame,
+                                           Schedule schedule,
+                                           Status status) {
+    //TODO
+    // receive via channel
+  }
+  
+  /**
+   * Forwards a request for the task frame associated with the given task command.
+   *
+   * @param taskCommand the task command
+   */
+  public void forwardFetchTaskFrame (TaskCommand taskCommand) {
+    //TODO
+    // send via channel
+  }
+  
+  /**
+   * Forwards the schedule corresponding to the given controlled resources and
+   * task command.
+   *
+   * @param controlledResources the given list of controlled resources
+   * @param taskCommand the given task command
+   * @param schedule the schedule
+   */
+  public void forwardSchedule (ArrayList controlledResources,
+                               TaskCommand taskCommand,
+                               Schedule schedule) {
+    //TODO
+    // send via channel
+  }
+  
+  /**
+   * Forwards the schedule corresponding to the given controlled resources and
+   * task command.
+   *
+   * @param controlledResources the given list of controlled resources
+   * @param taskCommand the given task command
+   * @param schedule the schedule
+   */
+  public void forwardPostSchedule (ArrayList controlledResources,
+                                  TaskCommand taskCommand,
+                                  Schedule schedule) {
+    //TODO
+    // send via channel
+  }
+  
+  /**
+   * Commands a subnode to perform the given task command using the given
+   * controlled resources.
+   *
+   * @param controlledResources the given list of controlled resources
+   * @param taskCommand the given task command
+   */
+  public void doSubTask (ArrayList controlledResources,
+                         TaskCommand taskCommand) {
+    //TODO
+  }
+  
+  /**
+   * Gets the behavior generation status.
+   *
+   * @param status the behavior generation status
+   */
+  public void behaviorGenerationStatus (Status status) {
+    //TODO
+  }
+  
   /**
    * Returns a string representation of this object.
    * 
@@ -116,4 +243,24 @@ public class BehaviorGeneration extends NodeComponent {
   public void setChildrenBehaviorGeneration(ArrayList childrenBehaviorGeneration) {
     this.childrenBehaviorGeneration = childrenBehaviorGeneration;
   }
+  
+  //// Protected Area
+  
+  /** the commanded goal for generated behavior to achieve */
+  protected Goal commandedGoal;
+
+  /**
+   * the parent node's BehaviorGeneration object.  The topmost
+   * BehaviorGeneration object has a value null here.
+   */
+  protected BehaviorGeneration parentBehaviorGeneration;
+
+  /**
+   * the child nodes' BehaviorGeneration objects.  The lowest level
+   * BehavoriGeneration object has a value null here.
+   */
+  protected ArrayList childrenBehaviorGeneration;
+  
+  //// Private Area
+  //// Internal Rep
 }

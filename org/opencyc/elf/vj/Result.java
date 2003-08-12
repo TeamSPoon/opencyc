300ce482dbbd18103a7deeeed00d74797f355804
stephenreed <>
2003-08-12 20:44:39 +0000

    Initial check in

---
+++ org/opencyc/elf/vj/Result.java
@@ -0,0 +1,36 @@
+package org.opencyc.elf.vj;
+
+//// Internal Imports
+
+//// External Imports
+
+/**
+ * <P>Result contains the result of a value judgement.
+ *
+ * <P>Copyright (c) 2003 Cycorp, Inc.  All rights reserved.
+ * <BR>This software is the proprietary information of Cycorp, Inc.
+ * <P>Use is subject to license terms.
+ *
+ * @author reed
+ * @date August 12, 2003, 2:43 PM
+ * @version $Id$
+ */
+public class Result {
+  
+  //// Constructors
+  
+  /** Creates a new instance of Result. */
+  public Result() {
+  }
+  
+  //// Public Area
+  
+  //// Protected Area
+  
+  //// Private Area
+  
+  //// Internal Rep
+  
+  //// Main
+  
+}

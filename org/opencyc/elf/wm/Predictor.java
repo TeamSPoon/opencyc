880bb154eac0c9d68f74ec05094fb9b5dfa5593e
stephenreed <>
2003-08-19 13:47:42 +0000

    initial check in - will not compile

---
+++ org/opencyc/elf/wm/Predictor.java
@@ -0,0 +1,187 @@
+package org.opencyc.elf.wm;
+
+//// Internal Imports
+import org.opencyc.elf.NodeComponent;
+
+import org.opencyc.elf.message.GenericMsg;
+import org.opencyc.elf.message.PerceivedSensoryInputMsg;
+import org.opencyc.elf.message.PredictedInputMsg;
+
+//// External Imports
+
+import EDU.oswego.cs.dl.util.concurrent.Executor;
+import EDU.oswego.cs.dl.util.concurrent.Puttable;
+import EDU.oswego.cs.dl.util.concurrent.Takable;
+import EDU.oswego.cs.dl.util.concurrent.ThreadedExecutor;
+
+/**
+ * Provides the predictor for the ELF WorldModel.<br>
+ * 
+ * @version $Id: SimulatorPredictor.java,v 1.1 2002/11/17 03:08:17 stephenreed
+ *          Exp $
+ * @author Stephen L. Reed  
+ * <p>Copyright 2001 Cycorp, Inc., license is open source GNU LGPL.
+ * <p><a href="http://www.opencyc.org/license.txt">the license</a>
+ * <p><a href="http://www.opencyc.org">www.opencyc.org</a>
+ * <p><a href="http://www.sourceforge.net/projects/opencyc">OpenCyc at SourceForge</a>
+ * <p>
+ * THIS SOFTWARE AND KNOWLEDGE BASE CONTENT ARE PROVIDED ``AS IS'' AND
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OPENCYC
+ * ORGANIZATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE AND KNOWLEDGE
+ * BASE CONTENT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+public class Predictor extends NodeComponent {
+  
+  /** Creates a new instance of Predictor */
+  public Predictor() {
+  }
+  
+  /** 
+   * Creates a new instance of Predictor with the given
+   * input and output message channels.
+   *
+   * @param predictorChannel the takable channel from which messages are input
+   * @param planEvaluationChannel the puttable channel to which messages are output
+   */
+  public Predictor (Takable predictorChannel,
+                    Puttable planEvaluatorChannel) {
+    consumer = new Consumer(predictorChannel,
+                            planEvaluatorChannel,
+                            this);
+    executor = new ThreadedExecutor();
+    try {
+      executor.execute(consumer);
+    }
+    catch (InterruptedException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+  }
+  
+  //// Public Area
+  
+  //// Protected Area
+  
+  /**
+   * Thread which processes the input message channel.
+   */
+  protected class Consumer implements Runnable {
+    
+    /**
+     * the takable channel from which messages are input
+     */
+    protected final Takable predictorChannel;
+    
+    /**
+     * the puttable channel to which messages are output for the plan evaluator
+     */
+    protected final Puttable planEvaluatorChannel;
+    
+    /**
+     * the parent node component
+     */
+    protected NodeComponent nodeComponent;
+          
+    /**
+     * Creates a new instance of Consumer.
+     *
+     * @param predictorChannel the takable channel from which messages are input
+     * @param planEvaluatorChannel the puttable channel to which messages are output
+     * @param nodeComponent the parent node component
+     */
+    protected Consumer (Takable predictorChannel,
+                        Puttable planEvaluatorChannel,
+                        NodeComponent nodeComponent) { 
+      this.predictorChannel = predictorChannel;
+      this.planEvaluatorChannel = planEvaluatorChannel;
+      this.nodeComponent = nodeComponent;
+    }
+
+    /**
+     * Reads messages from the input queue and processes them.
+     */
+    public void run () {
+      try {
+        while (true) { 
+          dispatchMsg((GenericMsg) predictorChannel.take()); 
+        }
+      }
+      catch (InterruptedException ex) {}
+    }
+      
+    /**
+     * Dispatches the given input channel message by type.
+     *
+     * @param genericMsg the given input channel message
+     */
+    void dispatchMsg (GenericMsg genericMsg) {
+      if (genericMsg instanceof PredictedInputMsg)
+        processPredictedInputMsg((PredictedInputMsg) genericMsg);
+      else if (genericMsg instanceof SimulateScheduleMsg)
+        processSimulateScheduleMsg((SimulateScheduleMsg) genericMsg);
+    }
+  
+    /**
+     * Simulates the schedule from an executor and sends the result to the plan evaluator.
+     *
+     * @param simulateScheduleMsg the simulate schedule message
+     */
+    protected void processSimulateScheduleMsg(SimulateScheduleMsg simulateScheduleMsg) {
+      controlledResources =  simulateScheduleMsg.getControlledResources();
+      taskCommand =  simulateScheduleMsg.getTaskCommand();
+      schedule =  simulateScheduleMsg.getSchedule();
+      //TODO
+    }
+    
+    /**
+     * Processes the predicted input message.
+     *
+     * @param predictedInputMsg the predicted input message
+     */
+    protected void processPredictedInputMsg(PredictedInputMsg predictedInputMsg) {
+      Object obj = predictedInputMsg.getObj();
+      Object data = predictedInputMsg.getData();
+      //TODO
+    }
+    
+    /**
+     * Sends the evaluate schedule message to the plan evaluator.
+     */
+    protected void sendEvaluateScheduleMsg() {
+      //TODO
+      Object result = null;
+      
+      EvaluateScheduleMsg evaluateScheduleMsg = new EvaluateScheduleMsg();
+      evaluateScheduleMsg.setSender(nodeComponent);
+      evaluateScheduleMsg.setControlledResources(controlledResources);
+      evaluateScheduleMsg.setTaskCommand(taskCommand);
+      evaluateScheduleMsg.setSchedule(schedule);
+      sendMsgToRecipient(planEvaluatorChannel, evaluateScheduleMsg);
+    }
+  }
+  
+  //// Private Area
+  
+  //// Internal Rep
+  
+  /**
+   * the thread which processes the input channel of messages
+   */
+  Consumer consumer;
+  
+  /**
+   * the executor of the consumer thread
+   */
+  Executor executor;
+  
+  //// Main
+
+}

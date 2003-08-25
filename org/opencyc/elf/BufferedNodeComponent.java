1d9e1b303346061a5a6a1f35244c33adfec19226
stephenreed <>
2003-08-25 13:54:55 +0000

    Added class comment

--- org/opencyc/elf/BufferedNodeComponent.java
+++ org/opencyc/elf/BufferedNodeComponent.java
@@ -9,9 +9,7 @@ import EDU.oswego.cs.dl.util.concurrent.Puttable;
 
 /**
  * Provides common attributes and behavior for Elementary Loop Functioning
- * (ELF) node components.<br>
- * Each node component contains one or more process threads that communicates with other 
- * node components by sending asychronous messages.
+ * (ELF) node components which have an input message buffer.
  * 
  * @version $Id$
  * @author Stephen L. Reed  

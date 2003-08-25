64eaab59b8e73203cfee5ce14c4ba5deb5c287ed
stephenreed <>
2003-08-25 18:16:02 +0000

    Removed messaging that will now be implemented as synchronous calls

--- org/opencyc/elf/wm/PlanSimulator.java
+++ org/opencyc/elf/wm/PlanSimulator.java
@@ -7,20 +7,7 @@ import org.opencyc.elf.bg.planner.Schedule;
 
 import org.opencyc.elf.bg.taskframe.TaskCommand;
 
-import org.opencyc.elf.message.GenericMsg;
-import org.opencyc.elf.message.EvaluateScheduleMsg;
-import org.opencyc.elf.message.PredictedInputMsg;
-import org.opencyc.elf.message.PredictionRequestMsg;
-import org.opencyc.elf.message.SimulateScheduleMsg;
-import org.opencyc.elf.message.SimulationFailureNotificationMsg;
-
 //// External Imports
-
-import EDU.oswego.cs.dl.util.concurrent.Executor;
-import EDU.oswego.cs.dl.util.concurrent.Puttable;
-import EDU.oswego.cs.dl.util.concurrent.Takable;
-import EDU.oswego.cs.dl.util.concurrent.ThreadedExecutor;
-
 import java.util.ArrayList;
 
 /**
@@ -57,213 +44,22 @@ public class PlanSimulator extends NodeComponent {
   public PlanSimulator() {
   }
 
-  /** 
-   * Creates a new instance of PlanSimulator with the given
-   * input and output message channels.
-   *
-     * @param planEvaluatorChannel the puttable channel to which messages are output for the plan
-     * evaluator
-     * @param predictorChannel the puttable channel to which messages are output for the predictor
-   */
-  public PlanSimulator (Takable planSimulatorChannel,
-                        Puttable planEvaluatorChannel,
-                        Puttable predictorChannel) {
-    consumer = new Consumer(planSimulatorChannel,
-                            planEvaluatorChannel,
-                            predictorChannel,
-                            this);
-    executor = new ThreadedExecutor();
-    try {
-      executor.execute(consumer);
-    }
-    catch (InterruptedException e) {
-      e.printStackTrace();
-      System.exit(1);
-    }
-  }
-  
- 
   //// Public Area
   
-  //// Protected Area
-  
   /**
-   * Thread which processes the input message channel.
+   * Returns a string representation of this object.
+   * 
+   * @return a string representation of this object
    */
-  protected class Consumer implements Runnable {
-    
-    /**
-     * the takable channel from which messages are input
-     */
-    protected final Takable planSimulatorChannel;
-    
-    /**
-     * the puttable channel to which messages are output for the plan evaluator
-     */
-    protected final Puttable planEvaluatorChannel;
-    
-    /**
-     * the puttable channel to which messages are output for the predictor
-     */
-    protected final Puttable predictorChannel;
-    
-    /**
-     * the scheduler's input channel used for a failure
-     * notification message
-     */
-    protected Puttable schedulerChannel;
-    
-    /**
-     * the parent node component
-     */
-    protected NodeComponent nodeComponent;
-    
-    /**
-     * the resources controlled by this node
-     */
-    protected ArrayList controlledResources;
-    
-    /**
-     * the node's commanded task
-     */
-    protected TaskCommand taskCommand;
-    
-    /**
-     * the proposed schedule for evaluation
-     */
-    protected Schedule schedule;
-      
-    protected SimulateScheduleMsg simulateScheduleMsg;
-    
-    /**
-     * Creates a new instance of Consumer.
-     *
-     * @param planSimulatorChannel the takable channel from which messages are input
-     * @param planEvaluatorChannel the puttable channel to which messages are output for the plan
-     * evaluator
-     * @param predictorChannel the puttable channel to which messages are output for the predictor
-     * @param nodeComponent the parent node component
-     */
-    protected Consumer (Takable planSimulatorChannel,
-                        Puttable planEvaluatorChannel,
-                        Puttable predictorChannel,
-                        NodeComponent nodeComponent) { 
-      this.planSimulatorChannel = planSimulatorChannel;
-      this.planEvaluatorChannel = planEvaluatorChannel;
-      this.predictorChannel = predictorChannel;
-      this.nodeComponent = nodeComponent;
-    }
-
-    /**
-     * Reads messages from the input queue and processes them.
-     */
-    public void run () {
-      try {
-        while (true) { 
-          dispatchMsg((GenericMsg) planSimulatorChannel.take()); 
-        }
-      }
-      catch (InterruptedException ex) {}
-    }
-      
-    /**
-     * Dispatches the given input channel message by type.
-     *
-     * @param genericMsg the given input channel message
-     */
-    void dispatchMsg (GenericMsg genericMsg) {
-      if (genericMsg instanceof PredictedInputMsg)
-        processPredictedInputMsg((PredictedInputMsg) genericMsg);
-      else if (genericMsg instanceof SimulateScheduleMsg)
-        processSimulateScheduleMsg((SimulateScheduleMsg) genericMsg);
-    }
-  
-    /**
-     * Simulates the schedule from an executor and sends the result to the plan evaluator.
-     *
-     * @param simulateScheduleMsg the simulate schedule message
-     */
-    protected void processSimulateScheduleMsg(SimulateScheduleMsg simulateScheduleMsg) {
-      this.simulateScheduleMsg = simulateScheduleMsg;
-      controlledResources =  simulateScheduleMsg.getControlledResources();
-      taskCommand =  simulateScheduleMsg.getTaskCommand();
-      schedule =  simulateScheduleMsg.getSchedule();
-      schedulerChannel = simulateScheduleMsg.getReplyToChannel();
-      //TODO
-    }
-    
-    /**
-     * Requests a predicted input from the predictor.
-     */
-    protected void requestPredictedInput () {
-      //TODO
-      Object obj = null;
-      PredictedInputMsg predictedInputMsg = new PredictedInputMsg();
-      predictedInputMsg.setSender(nodeComponent);
-      predictedInputMsg.setReplyToChannel((Puttable) planSimulatorChannel);
-      predictedInputMsg.setObj(obj);
-      sendMsgToRecipient(predictorChannel, predictedInputMsg);
-    }
-    
-    
-    /**
-     * Processes the predicted input message.
-     *
-     * @param predictedInputMsg the predicted input message
-     */
-    protected void processPredictedInputMsg(PredictedInputMsg predictedInputMsg) {
-      Object obj = predictedInputMsg.getObj();
-      Object data = predictedInputMsg.getData();
-      //TODO
-    }
-    
-    /**
-     * Sends the evaluate schedule message to the plan evaluator.
-     */
-    protected void sendEvaluateScheduleMsg() {
-      //TODO
-      Object result = null;
-      
-      EvaluateScheduleMsg evaluateScheduleMsg = new EvaluateScheduleMsg();
-      evaluateScheduleMsg.setSender(nodeComponent);
-      evaluateScheduleMsg.setControlledResources(controlledResources);
-      evaluateScheduleMsg.setTaskCommand(taskCommand);
-      evaluateScheduleMsg.setSchedule(schedule);
-      sendMsgToRecipient(planEvaluatorChannel, evaluateScheduleMsg);
-    }
-
-    /**
-     * Sends the simulation failure notification message back to
-     * the scheduler which requested the plan simulation.
-     */
-    protected void sendSimulationFailureNotificationMsg() {
-      //TODO
-      Object result = null;
-      
-      SimulationFailureNotificationMsg simulationFailureNotificationMsg = 
-        new SimulationFailureNotificationMsg();
-      simulationFailureNotificationMsg.setSender(nodeComponent);
-      simulationFailureNotificationMsg.setInReplyToMsg(simulateScheduleMsg);
-      simulationFailureNotificationMsg.setControlledResources(controlledResources);
-      simulationFailureNotificationMsg.setTaskCommand(taskCommand);
-      simulationFailureNotificationMsg.setSchedule(schedule);
-      sendMsgToRecipient(schedulerChannel, simulationFailureNotificationMsg);
-    }
+  public String toString() {
+    return "PlanSimulator for " + node.getName();
   }
-  
+
+  //// Protected Area
+    
   //// Private Area
   
-  //// Internal Rep
-  
-  /**
-   * the thread which processes the input channel of messages
-   */
-  Consumer consumer;
-  
-  /**
-   * the executor of the consumer thread
-   */
-  Executor executor;
+  //// Internal Rep  
   
   //// Main
 }

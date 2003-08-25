32fcecf3ef3df87cca5232b5acc4853318b32695
stephenreed <>
2003-08-25 14:04:03 +0000

    initial check in

---
+++ org/opencyc/elf/message/ExecutorStatusMsg.java
@@ -0,0 +1,96 @@
+package org.opencyc.elf.message;
+
+//// Internal Imports
+
+import org.opencyc.elf.Status;
+
+//// External Imports
+
+/**
+ * <P>Provides the status message that is sent from the executor to the scheduler.
+ *
+ * @version $Id$
+ * @author  reed
+ *
+ * <p>Copyright 2001 Cycorp, Inc., license is open source GNU LGPL.
+ * <p><a href="http://www.opencyc.org/license.txt">the license</a>
+ * <p><a href="http://www.opencyc.org">www.opencyc.org</a>
+ * <p><a href="http://www.sourceforge.net/projects/opencyc">OpenCyc at SourceForge</a>
+ * <p>
+ * THIS SOFTWARE AND KNOWLEDGE BASE CONTENT ARE PROVIDED ``AS IS'' AND
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OPENCYC
+ * ORGANIZATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE AND KNOWLEDGE
+ * BASE CONTENT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+public class ExecutorStatusMsg extends GenericMsg {
+  
+  //// Constructors
+  
+  /** Creates a new instance of ExecutorStatusMsg */
+  public ExecutorStatusMsg() {
+  }
+  
+  //// Public Area
+  
+  /**
+   * Returns true if the given object equals this object.
+   *
+   * @param obj the given object
+   * @return true if the given object equals this object
+   */
+  public boolean equals(Object obj) {
+    if (! (obj instanceof Class))
+      return false;
+    //TODO
+    return true;
+  }
+  
+  /**
+   * Returns a string representation of this object.
+   *
+   * @return a string representation of this object
+   */
+  public String toString() {
+    //TODO
+    return "";
+  }
+  
+  /**
+   * Gets the executor status
+   *
+   * @return the executor status
+   */
+  public Status getStatus () {
+    return status;
+  }
+
+  /**
+   * Sets the executor status
+   *
+   * @param status the sexecutor status
+   */
+  public void setStatus (Status status) {
+    this.status = status;
+  }
+
+  //// Protected Area
+  
+  //// Private Area
+  
+  //// Internal Rep
+  /**
+   * the scheduler status
+   */
+  protected Status status;
+  
+  //// Main
+  
+}
---
+++ org/opencyc/elf/message/ReplanMsg.java
@@ -0,0 +1,178 @@
+package org.opencyc.elf.message;
+
+//// Internal Imports
+
+import org.opencyc.elf.Result;
+
+import org.opencyc.elf.bg.planner.Schedule;
+
+import org.opencyc.elf.bg.taskframe.TaskCommand;
+
+//// External Imports
+
+import java.util.ArrayList;
+
+/**
+ * <P>Contains the ELF replan message that is sent from the plan selector to the
+ * scheduler and to the job assigner.
+ *
+ * @version $Id$
+ * @author  reed
+ *
+ * <p>Copyright 2001 Cycorp, Inc., license is open source GNU LGPL.
+ * <p><a href="http://www.opencyc.org/license.txt">the license</a>
+ * <p><a href="http://www.opencyc.org">www.opencyc.org</a>
+ * <p><a href="http://www.sourceforge.net/projects/opencyc">OpenCyc at SourceForge</a>
+ * <p>
+ * THIS SOFTWARE AND KNOWLEDGE BASE CONTENT ARE PROVIDED ``AS IS'' AND
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OPENCYC
+ * ORGANIZATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE AND KNOWLEDGE
+ * BASE CONTENT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+public class ReplanMsg extends GenericMsg {
+  
+  //// Constructors
+  
+  /** Creates a new instance of ReplanMsg */
+  public ReplanMsg() {
+  }
+  
+  //// Public Area
+  
+  /**
+   * Returns true if the given object equals this object.
+   *
+   * @param obj the given object
+   * @return true if the given object equals this object
+   */
+  public boolean equals(Object obj) {
+    if (! (obj instanceof Class))
+      return false;
+    //TODO
+    return true;
+  }
+  
+  /**
+   * Returns a string representation of this object.
+   *
+   * @return a string representation of this object
+   */
+  public String toString() {
+    //TODO
+    return "";
+  }
+  
+  /**
+   * Gets the resources controlled by this node
+   *
+   * @return the resources controlled by this node
+   */
+  public ArrayList getControlledResources () {
+    return controlledResources;
+  }
+
+  /**
+   * Sets the resources controlled by this node
+   *
+   * @param controlledResources the resources controlled by this node
+   */
+  public void setControlledResources (ArrayList controlledResources) {
+    this.controlledResources = controlledResources;
+  }
+
+  /**
+   * Gets the node's commanded task
+   *
+   * @return the node's commanded task
+   */
+  public TaskCommand getTaskCommand () {
+    return taskCommand;
+  }
+
+  /**
+   * Sets the node's commanded task
+   *
+   * @param taskCommandthe node's commanded task
+   */
+  public void setTaskCommand (TaskCommand taskCommand) {
+    this.taskCommand = taskCommand;
+  }
+
+  /**
+   * Gets a proposed schedule of actions to carry out the commanded task using
+   * the node's contolled resources
+   *
+   * @return a proposed schedule of actions to carry out the commanded task using
+   * the node's contolled resources
+   */
+  public Schedule getSchedule () {
+    return schedule;
+  }
+
+  /**
+   * Sets a proposed schedule of actions to carry out the commanded task using
+   * the node's contolled resources
+   *
+   * @param schedule a proposed schedule of actions to carry out the commanded task using
+   * the node's contolled resources
+   */
+  public void setSchedule (Schedule schedule) {
+    this.schedule = schedule;
+  }
+
+  /**
+   * Gets the schedule evaluation result
+   *
+   * @return the schedule evaluation result
+   */
+  public Result getResult () {
+    return result;
+  }
+
+  /**
+   * Sets the schedule evaluation result
+   *
+   * @param result the schedule evaluation result
+   */
+  public void setResult (Result result) {
+    this.result = result;
+  }
+  
+  //// Protected Area
+  
+  //// Private Area
+  
+  //// Internal Rep
+  
+  /**
+   * the resources controlled by this node
+   */
+  protected ArrayList controlledResources;
+  
+  /**
+   * the node's commanded task
+   */
+  protected TaskCommand taskCommand;
+  
+  /**
+   * a proposed schedule of actions to carry out the commanded task using
+   * the node's contolled resources
+   */
+  protected Schedule schedule;
+  
+  /**
+   * the schedule evaluation result
+   */
+  protected Result result;
+  
+  //// Main
+  
+}

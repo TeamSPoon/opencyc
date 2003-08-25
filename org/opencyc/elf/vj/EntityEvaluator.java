bd67211034c72a6894060412a23beaf205c03404
stephenreed <>
2003-08-25 18:05:03 +0000

    Removed messaging methods as calls will be synchronous

--- org/opencyc/elf/vj/EntityEvaluator.java
+++ org/opencyc/elf/vj/EntityEvaluator.java
@@ -3,16 +3,7 @@ package org.opencyc.elf.vj;
 //// Internal Imports
 import org.opencyc.elf.NodeComponent;
 
-import org.opencyc.elf.message.GenericMsg;
-import org.opencyc.elf.message.KBObjectPutMsg;
-import org.opencyc.elf.message.PerceivedSensoryInputMsg;
-
 //// External Imports
-
-import EDU.oswego.cs.dl.util.concurrent.Executor;
-import EDU.oswego.cs.dl.util.concurrent.Puttable;
-import EDU.oswego.cs.dl.util.concurrent.Takable;
-import EDU.oswego.cs.dl.util.concurrent.ThreadedExecutor;
 
 /**
  * <P>EntityEvaluator is designed to evaluate perceived sensory inputs and update
@@ -47,119 +38,12 @@ public class EntityEvaluator extends NodeComponent {
   public EntityEvaluator() {
   }
   
-  /** 
-   * Creates a new instance of EntityEvaluator with the given
-   * input and output message channels.
-   *
-   * @param entityEvaluatorChannel the takable channel from which messages are input
-   * @param knowledgeBaseChannel the puttable channel to which messages are output
-   */
-  public EntityEvaluator(Takable entityEvaluatorChannel,
-                         Puttable knowledgeBaseChannel) {
-    consumer = new Consumer(entityEvaluatorChannel,
-                            knowledgeBaseChannel,
-                            this);
-    executor = new ThreadedExecutor();
-    try {
-      executor.execute(consumer);
-    }
-    catch (InterruptedException e) {
-      e.printStackTrace();
-      System.exit(1);
-    }
-  }
-  
-  //// Public Area
-  
   //// Protected Area
-  
-  /**
-   * Thread which processes the input message channel.
-   */
-  protected class Consumer implements Runnable {
-    
-    /**
-     * the takable channel from which messages are input
-     */
-    protected final Takable entityEvaluatorChannel;
-    
-    /**
-     * the puttable channel to which messages are output for the knowledge base
-     */
-    protected final Puttable knowledgeBaseChannel;
-    
-    /**
-     * the parent node component
-     */
-    protected NodeComponent nodeComponent;
-    
-    /**
-     * Creates a new instance of Consumer.
-     *
-     * @param entityEvaluatorChannel the takable channel from which messages are input
-     * @param knowledgeBaseChannel the puttable channel to which messages are output
-     * @param nodeComponent the parent node component
-     */
-    protected Consumer (Takable entityEvaluatorChannel,
-                        Puttable knowledgeBaseChannel,
-                        NodeComponent nodeComponent) { 
-      this.entityEvaluatorChannel = entityEvaluatorChannel;
-      this.knowledgeBaseChannel = knowledgeBaseChannel;
-      this.nodeComponent = nodeComponent;
-    }
-
-    /**
-     * Reads messages from the input queue and processes them.
-     */
-    public void run () {
-      try {
-        while (true) { 
-          processPerceivedSensoryInputMsg((PerceivedSensoryInputMsg) entityEvaluatorChannel.take()); 
-        }
-      }
-      catch (InterruptedException ex) {}
-    }
-      
-    /**
-     * Processes the perceived sensory input message received from a next level lower sensory
-     * processing node component.
-     */
-    protected void processPerceivedSensoryInputMsg(PerceivedSensoryInputMsg perceivedSensoryInputMsg) {
-      Object obj = perceivedSensoryInputMsg.getObj();
-      Object data = perceivedSensoryInputMsg.getData();
-      //TODO
-    }
-    
-    /**
-     * Sends the knowledge base object put message that updates the entity's worth.
-     */
-    protected void sendKBObjectPutMsg() {
-      //TODO
-      Object obj = null;
-      Object data = null;
-      
-      KBObjectPutMsg kbObjectPutMsg = new KBObjectPutMsg();
-      kbObjectPutMsg.setSender(nodeComponent);
-      kbObjectPutMsg.setObj(obj);
-      kbObjectPutMsg.setData(data);
-      sendMsgToRecipient(knowledgeBaseChannel, kbObjectPutMsg);
-    }
-  }
   
   //// Private Area
   
   //// Internal Rep
-  
-  /**
-   * the thread which processes the input channel of messages
-   */
-  Consumer consumer;
-  
-  /**
-   * the executor of the consumer thread
-   */
-  Executor executor;
-  
+    
   //// Main
   
 }

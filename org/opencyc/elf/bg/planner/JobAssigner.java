1066bed46fbf08efe94b3c486fbbe23b576c9167
stephenreed <>
2003-08-28 20:29:30 +0000

    cvs comment

--- org/opencyc/elf/bg/planner/JobAssigner.java
+++ org/opencyc/elf/bg/planner/JobAssigner.java
@@ -30,8 +30,7 @@ import EDU.oswego.cs.dl.util.concurrent.ThreadedExecutor;
  * among the available agents and resources.
  * </p>
  * 
- * @version $Id: BehaviorGeneration.java,v 1.3 2002/11/19 02:42:53 stephenreed
- *          Exp $
+ * @version $Id$
  * @author Stephen L. Reed  
  * <p>Copyright 2001 Cycorp, Inc., license is open source GNU LGPL.
  * <p><a href="http://www.opencyc.org/license.txt">the license</a>

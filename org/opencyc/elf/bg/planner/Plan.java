a1431ec8874f2809efd5c0b5b92d4f8ad4582c9a
stephenreed <>
2003-08-28 20:30:29 +0000

    cvs tag comment

--- org/opencyc/elf/bg/planner/Plan.java
+++ org/opencyc/elf/bg/planner/Plan.java
@@ -14,8 +14,7 @@ import org.opencyc.elf.bg.state.State;
  * of subgoals leading from the starting state to the goal state.
  * </p>
  * 
- * @version $Id: BehaviorGeneration.java,v 1.3 2002/11/19 02:42:53 stephenreed
- *          Exp $
+ * @version $Id$
  * @author Stephen L. Reed  
  * <p>Copyright 2001 Cycorp, Inc., license is open source GNU LGPL.
  * <p><a href="http://www.opencyc.org/license.txt">the license</a>

b1dbd686fa4c92c18804ff1a924ac6a25ddabd63
stephenreed <>
2003-08-28 20:31:21 +0000

    cvs comment tag

--- org/opencyc/elf/bg/planner/Resource.java
+++ org/opencyc/elf/bg/planner/Resource.java
@@ -12,8 +12,7 @@ import org.opencyc.elf.bg.state.State;
  * allocated to an agent in order for that perform a given task.
  * </p>
  * 
- * @version $Id: BehaviorGeneration.java,v 1.3 2002/11/19 02:42:53 stephenreed
- *          Exp $
+ * @version $Id$
  * @author Stephen L. Reed  
  * <p>Copyright 2001 Cycorp, Inc., license is open source GNU LGPL.
  * <p><a href="http://www.opencyc.org/license.txt">the license</a>

21d2f27629db327135324c0188aea23c14b3dd67
stephenreed <>
2003-08-29 02:06:30 +0000

    initial check in

---
+++ org/opencyc/elf/bg/procedure/ProcedureFactory.java
@@ -0,0 +1,91 @@
+package org.opencyc.elf.bg.procedure;
+
+//// Internal Imports
+
+//// External Imports
+
+/**
+ * <P>
+ * ProcedureFactory instantiates the ELF procedures.
+ * </p>
+ * 
+ * @version $Id$
+ * @author Stephen L. Reed  
+ * <p>Copyright 2001 Cycorp, Inc., license is open source GNU LGPL.
+ * <p><a href="http://www.opencyc.org/license.txt">the license</a>
+ * <p><a href="http://www.opencyc.org">www.opencyc.org</a>
+ * <p><a href="http://www.sourceforge.net/projects/opencyc">OpenCyc at SourceForge</a>
+ * <p>
+ * THIS SOFTWARE AND KNOWLEDGE BASE CONTENT ARE PROVIDED ``AS IS'' AND
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OPENCYC
+ * ORGANIZATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE AND KNOWLEDGE
+ * BASE CONTENT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+public class ProcedureFactory {
+  
+  //// Constructors
+  
+  /** Creates a new instance of ProcedureFactory. */
+  public ProcedureFactory() {
+  }
+  
+  //// Public Area
+  
+  /**
+   * Instantiates the ELF procedures.
+   */
+  public void instantiate () {
+  }
+  
+  //// Protected Area
+  
+  /**
+   * Instantiates the emergency stop procedure.
+   */
+  protected void instantiateEmergencyStop () {
+    //TODO
+  }
+  
+  /**
+   * Instantiates the init procedure.
+   */
+  protected void instantiateInit () {
+    //TODO
+  }
+  
+  /**
+   * Instantiates the pause procedure.
+   */
+  protected void instantiatePause () {
+    //TODO
+  }
+  
+  /**
+   * Instantiates the resume procedure.
+   */
+  protected void instantiateResume () {
+    //TODO
+  }
+  
+  /**
+   * Instantiates the abort procedure.
+   */
+  protected void instantiateAbort () {
+    //TODO
+  }
+  
+  //// Private Area
+  
+  //// Internal Rep
+  
+  //// Main
+  
+}

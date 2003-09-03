8aea659daffbb1b8d6f39cec60ebb6b91a5fabd3
stephenreed <>
2003-09-03 21:32:46 +0000

    removed unneeded instance variables

--- org/opencyc/elf/goal/Goal.java
+++ org/opencyc/elf/goal/Goal.java
@@ -1,8 +1,6 @@
 package org.opencyc.elf.goal;
 
 //// Internal Imports
-import org.opencyc.cycobject.CycAssertion;
-import org.opencyc.cycobject.CycFort;
 
 import org.opencyc.elf.ELFObject;
 
@@ -13,7 +11,6 @@ import java.util.ArrayList;
 
 /**
  * Provides the Goal container for the Elementary Loop Functioning (ELF).
- * Either a goal state or a goal procedure is specified.<br>
  * 
  * @version $Id$
  * @author Stephen L. Reed  
@@ -72,42 +69,6 @@ public class Goal extends ELFObject {
   }
 
   /**
-   * Gets the goal sentence stated in CycL
-   * 
-   * @return the goal sentence stated in CycL
-   */
-  public CycAssertion getGoalSentence() {
-    return goalSentence;
-  }
-
-  /**
-   * Sets the goal sentence stated in CycL
-   * 
-   * @param goalSentence the goal sentence stated in CycL
-   */
-  public void setGoalSentence(CycAssertion goalSentence) {
-    this.goalSentence = goalSentence;
-  }
-
-  /**
-   * Gets the goal stated in natural language
-   * 
-   * @return the goal stated in natural language
-   */
-  public String getGoalComment() {
-    return goalComment;
-  }
-
-  /**
-   * Sets the goal stated in natural language
-   * 
-   * @param goalComment the goal stated in natural language
-   */
-  public void setGoalComment(String goalComment) {
-    this.goalComment = goalComment;
-  }
-
-  /**
    * Gets the list of states which if entered, indicate goal failure
    * 
    * @return the list of states which if entered, indicate goal failure
@@ -127,42 +88,6 @@ public class Goal extends ELFObject {
   }
 
   /**
-   * Gets the parent goal of this goal
-   * 
-   * @return the parent goal of this goal
-   */
-  public Goal getParent() {
-    return parent;
-  }
-
-  /**
-   * Sets the parent goal of this goal
-   * 
-   * @param parent the parent goal of this goal
-   */
-  public void setParent(Goal parent) {
-    this.parent = parent;
-  }
-
-  /**
-   * Gets the subgoals of this goal
-   * 
-   * @return the subgoals of this goal
-   */
-  public ArrayList getSubGoals() {
-    return subGoals;
-  }
-
-  /**
-   * Sets the subgoals of this goal
-   * 
-   * @param subGoals the subgoals of this goal
-   */
-  public void setSubGoals(ArrayList subGoals) {
-    this.subGoals = subGoals;
-  }
-
-  /**
    * Gets the goal importance
    * 
    * @return the goal importance
@@ -179,24 +104,6 @@ public class Goal extends ELFObject {
   public void setImportance(Importance importance) {
     this.importance = importance;
   }
-
-  /**
-   * Gets the context of the goal
-   * 
-   * @return the context of the goal
-   */
-  public CycFort getContext() {
-    return context;
-  }
-
-  /**
-   * Sets the context of the goal
-   * 
-   * @param context the context of the goal
-   */
-  public void setContext(CycFort context) {
-    this.context = context;
-  }
   
   //// Protected Area
   
@@ -207,26 +114,11 @@ public class Goal extends ELFObject {
   /** the goal state */
   protected State goalState;
 
-  /** the goal sentence stated in CycL */
-  protected CycAssertion goalSentence;
-
-  /** the goal stated in natural language */
-  protected String goalComment;
-
   /** the list of states which if entered, indicate goal failure */
   protected ArrayList goalFailureStates;
 
-  /** the parent goal of this goal */
-  protected Goal parent;
-
-  /** the subgoals of this goal */
-  protected ArrayList subGoals = new ArrayList();
-
   /** the goal importance */
   protected Importance importance;
-
-  /** the context of the goal */
-  protected CycFort context;
 
   //// Main
 }

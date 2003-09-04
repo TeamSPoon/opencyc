7101c15363898001abe1d94ec68b9849b55b9c46
stephenreed <>
2003-09-04 00:08:49 +0000

    initial check in

---
+++ org/opencyc/elf/goal/GoalEvaluator.java
@@ -0,0 +1,65 @@
+package org.opencyc.elf.goal;
+
+//// Internal Imports
+
+//// External Imports
+
+/**
+ * <P>GoalEvaluator evaluates 
+ *
+ * <P>Copyright (c) 2003 Cycorp, Inc.  All rights reserved.
+ * <BR>This software is the proprietary information of Cycorp, Inc.
+ * <P>Use is subject to license terms.
+ *
+ * @author Stephen L. Reed  
+ * <p>Copyright 2001 Cycorp, Inc., license is open source GNU LGPL.
+ * <p><a href="http://www.opencyc.org/license.txt">the license</a>
+ * <p><a href="http://www.opencyc.org">www.opencyc.org</a>
+ * <p><a href="http://www.sourceforge.net/projects/opencyc">OpenCyc at SourceForge</a>
+ * <p>
+ * THIS SOFTWARE AND KNOWLEDGE BASE CONTENT ARE PROVIDED ``AS IS'' AND
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OPENCYC
+ * ORGANIZATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE AND KNOWLEDGE
+ * BASE CONTENT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * @date August 11, 2003, 2:57 PM
+ * @version $Id$
+ */
+public class GoalEvaluator {
+  
+  //// Constructors
+  
+  /** Creates a new instance of GoalEvaluator. */
+  public GoalEvaluator() {
+  }
+  
+  //// Public Area
+  
+  /**
+   * goal achieved when the only argument, a state variable, is not null;
+   */
+  public static final int NOT_NULL = 1;
+
+  /**
+   * Evaluates a predicate expression.
+   */
+  public boolean evaluate (GoalPredicateExpression goalPredicateExpression) {
+  }
+ 
+
+  //// Protected Area
+  
+  //// Private Area
+  
+  //// Internal Rep
+  
+  //// Main
+  
+}
---
+++ org/opencyc/elf/goal/GoalFactory.java
@@ -0,0 +1,76 @@
+package org.opencyc.elf.goal;
+
+//// Internal Imports
+
+//// External Imports
+
+/**
+ * <P>GoalFactory is designed to create goals.  There is a singleton instance.
+ *
+ * <P>Copyright (c) 2003 Cycorp, Inc.  All rights reserved.
+ * <BR>This software is the proprietary information of Cycorp, Inc.
+ * <P>Use is subject to license terms.
+ *
+ * @author Stephen L. Reed  
+ * <p>Copyright 2001 Cycorp, Inc., license is open source GNU LGPL.
+ * <p><a href="http://www.opencyc.org/license.txt">the license</a>
+ * <p><a href="http://www.opencyc.org">www.opencyc.org</a>
+ * <p><a href="http://www.sourceforge.net/projects/opencyc">OpenCyc at SourceForge</a>
+ * <p>
+ * THIS SOFTWARE AND KNOWLEDGE BASE CONTENT ARE PROVIDED ``AS IS'' AND
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OPENCYC
+ * ORGANIZATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE AND KNOWLEDGE
+ * BASE CONTENT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * @date August 11, 2003, 2:57 PM
+ * @version $Id$
+ */
+public class GoalFactory {
+  
+  //// Constructors
+  
+  /** Creates a new instance of GoalFactory. */
+  public GoalFactory() {
+    goalFactory = this;
+  }
+  
+  //// Public Area
+  
+  /**
+   * Gets the singleton goal factory instance.
+   *
+   * @return the singleton goal factory instance
+   */
+  public static GoalFactory getInstance () {
+    return goalFactory;
+  }
+  
+  /**
+   * Makes a new console prompted input goal that achieves a string return value.
+   */
+  public Goal makeConsolePromptedInput () {
+    Goal goal = new Goal();
+    
+  }  
+  
+  //// Protected Area
+  
+  //// Private Area
+  
+  //// Internal Rep
+  
+  /**
+   * the singleton goal factory instance
+   */
+  protected static GoalFactory goalFactory;
+  
+  //// Main
+  
+}

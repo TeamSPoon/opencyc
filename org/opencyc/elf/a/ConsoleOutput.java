1a5a74d1e8a83764222225899148ca24a3ec6970
stephenreed <>
2003-09-08 23:08:41 +0000

    implements Actuator

--- org/opencyc/elf/a/ConsoleOutput.java
+++ org/opencyc/elf/a/ConsoleOutput.java
@@ -35,7 +35,7 @@ import EDU.oswego.cs.dl.util.concurrent.ThreadedExecutor;
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE AND KNOWLEDGE
  * BASE CONTENT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-public class ConsoleOutput extends NodeComponent {
+public class ConsoleOutput extends NodeComponent implements Actuator {
   
   //// Constructors
 

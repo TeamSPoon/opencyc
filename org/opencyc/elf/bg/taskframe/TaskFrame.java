07f1a69a80dec6fb42046e7380d29a14c704eec1
stephenreed <>
2003-09-17 23:10:40 +0000

    added scheduleAlternatives, including conditionalScheduleSets as elements

--- org/opencyc/elf/bg/taskframe/TaskFrame.java
+++ org/opencyc/elf/bg/taskframe/TaskFrame.java
@@ -21,8 +21,7 @@ import java.util.List;
 
 import org.doomdark.uuid.UUID;
 
-/**
- * TaskFrame specifies the materials, tools, and procedures to accomplish a task.  Generic task
+/** TaskFrame specifies the materials, tools, and procedures to accomplish a task.  Generic task
  * frames are strored in the task frame library and from the library cloned instances are
  * provided to the job assigner when decomposing task commands.
  * 
@@ -56,28 +55,32 @@ public class TaskFrame {
 
   //// Public Area
 
-  /**
-   * Creates and returns a copy of this object as initialized by the task frame factory
+  /** Creates and returns a copy of this object as initialized by the task frame factory
    * when the task frame library is populated.  The remaining instance variables in the
    * cloned task frame are set by the job assinger according to the commanded task.
    */
   public Object clone () {
     TaskFrame taskFrame = new TaskFrame();
     taskFrame.setTaskName(taskName);
-    List clonedConditionalScheduleSets = new ArrayList();
-    Iterator iter = conditionalScheduleSets.iterator();
-    while (iter.hasNext()) {
-      ConditionalScheduleSet conditionalScheduleSet = (ConditionalScheduleSet) iter.next();
-      clonedConditionalScheduleSets.add((ConditionalScheduleSet) conditionalScheduleSet.clone());
+    List clonedScheduleAlternatives = new ArrayList();
+    Iterator alternativeIterator = scheduleAlternatives.iterator();
+    while (alternativeIterator.hasNext()) {
+      List conditionalScheduleSets = (List) alternativeIterator.next();
+      List clonedConditionalScheduleSets = new ArrayList();
+      Iterator iter = conditionalScheduleSets.iterator();
+      while (iter.hasNext()) {
+        ConditionalScheduleSet conditionalScheduleSet = (ConditionalScheduleSet) iter.next();
+        clonedConditionalScheduleSets.add((ConditionalScheduleSet) conditionalScheduleSet.clone());
+      }
+      clonedScheduleAlternatives.add(clonedConditionalScheduleSets);
     }
-    taskFrame.setConditionalScheduleSets(clonedConditionalScheduleSets);
+    taskFrame.setScheduleAlternatives(clonedScheduleAlternatives);
     taskFrame.setTaskAction((Action) taskAction.clone());
     taskFrame.setTaskGoal(taskGoal);    
     return taskFrame;
   }
   
-  /**
-   * Returns true if the given object equals this object.
+  /** Returns true if the given object equals this object.
    * 
    * @param obj the given object
    * 
@@ -91,8 +94,7 @@ public class TaskFrame {
     return this.taskId.equals(that.taskId);
   }
 
-  /**
-   * Returns a string representation of this object.
+  /** Returns a string representation of this object.
    * 
    * @return a string representation of this object
    */
@@ -107,8 +109,7 @@ public class TaskFrame {
     return stringBuffer.toString();
   }
 
-  /**
-   * Gets the task name which is either an action name or a goal name.
+  /** Gets the task name which is either an action name or a goal name.
    * 
    * @return the task name which is either an action name or a goal name
    */
@@ -116,8 +117,7 @@ public class TaskFrame {
     return taskName;
   }
 
-  /**
-   * Sets the task name which is either an action name or a goal name.
+  /** Sets the task name which is either an action name or a goal name.
    * 
    * @param taskName the task name which is either an action name or a goal name
    */
@@ -130,8 +130,7 @@ public class TaskFrame {
     taskId = new UUID();
   }
   
-  /**
-   * Gets the task identifier consisting of a unique id for each task commanded.
+  /** Gets the task identifier consisting of a unique id for each task commanded.
    * 
    * @return the task identifier consisting of a unique id for each task
    *         commanded
@@ -140,8 +139,7 @@ public class TaskFrame {
     return taskId;
   }
 
-  /**
-   * Gets the action to be accomplished by this task
+  /** Gets the action to be accomplished by this task
    *
    * @return the action to be accomplished by this task
    */
@@ -149,8 +147,7 @@ public class TaskFrame {
     return taskAction;
   }
 
-  /**
-   * Sets the action to be accomplished by this task
+  /** Sets the action to be accomplished by this task
    *
    * @param taskAction the action to be accomplished by this task
    */
@@ -158,8 +155,7 @@ public class TaskFrame {
     this.taskAction = taskAction;
   }
 
-  /**
-   * Gets the desired state to be acheived or maintained by this task
+  /** Gets the desired state to be acheived or maintained by this task
    * 
    * @return the desired state to be acheived or maintained by this
    *         task
@@ -168,8 +164,7 @@ public class TaskFrame {
     return taskGoal;
   }
 
-  /**
-   * Sets the desired state to be acheived or maintained by this task
+  /** Sets the desired state to be acheived or maintained by this task
    * 
    * @param taskGoal the desired state to be acheived or maintained by this
    *        task
@@ -178,8 +173,7 @@ public class TaskFrame {
     this.taskGoal = taskGoal;
   }
 
-  /**
-   * Gets the objects upon which the desired task is performed.
+  /** Gets the objects upon which the desired task is performed.
    * 
    * @return taskObjects the objects upon which the desired task is performed
    */
@@ -187,8 +181,7 @@ public class TaskFrame {
     return taskObjects;
   }
 
-  /**
-   * Sets the objects upon which the desired task is performed.
+  /** Sets the objects upon which the desired task is performed.
    * 
    * @param taskObjects the objects upon which the desired task is performed
    */
@@ -196,8 +189,7 @@ public class TaskFrame {
     this.taskObjects = taskObjects;
   }
 
-  /**
-   * Gets the parameters that specify or modulate how the task should be
+  /** Gets the parameters that specify or modulate how the task should be
    * performed
    * 
    * @return the parameters that specify or modulate how the
@@ -207,8 +199,7 @@ public class TaskFrame {
     return taskParameters;
   }
 
-  /**
-   * Sets the parameters that specify or modulate how the task should be
+  /** Sets the parameters that specify or modulate how the task should be
    * performed
    * 
    * @param taskParameters the parameters that specify or modulate how the task
@@ -218,69 +209,7 @@ public class TaskFrame {
     this.taskParameters = taskParameters;
   }
 
-  /**
-   * Gets all names of the actuators that are responsible for carrying out the task
-   * 
-   * @return all names of the actuators that are responsible for carrying out the task
-   */
-  public List getActuatorNames () {
-    List actuatorNames = new ArrayList();
-    Iterator conditionalScheduleSetIterator = conditionalScheduleSets.iterator();
-    while (conditionalScheduleSetIterator.hasNext()) {
-      ConditionalScheduleSet conditionalScheduleSet = (ConditionalScheduleSet) conditionalScheduleSetIterator.next();
-      Iterator actuatorNameIterator = conditionalScheduleSet.getActuatorNames().iterator();
-      while (actuatorNameIterator.hasNext()) {
-        String actuatorName = (String) actuatorNameIterator.next();
-        if (! actuatorNames.contains(actuatorName))
-          actuatorNames.add(actuatorName);
-      }
-    }
-    return actuatorNames;
-  }
-
-  /**
-   * Gets all the names of the sensors that are responsible for sensing phenomena related to the task
-   * 
-   * @return all the names of the sensors that are responsible for sensing phenomena related to the task
-   */
-  public List getSensorNames () {
-    List sensorNames = new ArrayList();
-    Iterator conditionalScheduleSetIterator = conditionalScheduleSets.iterator();
-    while (conditionalScheduleSetIterator.hasNext()) {
-      ConditionalScheduleSet conditionalScheduleSet = (ConditionalScheduleSet) conditionalScheduleSetIterator.next();
-      Iterator sensorNameIterator = conditionalScheduleSet.getSensorNames().iterator();
-      while (sensorNameIterator.hasNext()) {
-        String sensorName = (String) sensorNameIterator.next();
-        if (! sensorNames.contains(sensorName))
-          sensorNames.add(sensorName);
-      }
-    }
-    return sensorNames;
-  }
-
-  /**
-   * Gets all the resources which are required to execute the task.
-   * 
-   * @return all the resources which are required to execute the task
-   */
-  public List getTaskResources () {
-    List taskResources = new ArrayList();
-    Iterator actuatorNameIterator = getActuatorNames().iterator();
-    while (actuatorNameIterator.hasNext()) {
-      String actuatorName = (String) actuatorNameIterator.next();
-      Actuator actuator = ActuatorPool.getInstance().getActuator(actuatorName);
-      Iterator resourceIterator = actuator.getResources().iterator();
-      while (resourceIterator.hasNext()) {
-        Resource resource = (Resource) resourceIterator.next();
-        if (! taskResources.contains(resource))
-          taskResources.add(resource);
-      }
-    }
-    return taskResources;
-  }
-
-  /**
-   * Gets the constraints upon the performance of the task
+  /** Gets the constraints upon the performance of the task
    * 
    * @return the constraints upon the performance of the task
    */
@@ -288,8 +217,7 @@ public class TaskFrame {
     return taskConstraints;
   }
 
-  /**
-   * Sets the constraints upon the performance of the task
+  /** Sets the constraints upon the performance of the task
    * 
    * @param taskConstraints the constraints upon the performance of the task
    */
@@ -297,38 +225,30 @@ public class TaskFrame {
     this.taskConstraints = taskConstraints;
   }
 
-  /**
-   * Gets the schedule set condition list that associates a predicate expression with the set
-   * of schedules to carry out
+  /** Gets the schedule alternaives, each of which is a list of conditional schedule sets
    * 
-   * @return the schedule set condition list that associates a predicate expression with the set
-   * of schedules to carry out
+   * @return the schedule alternaives, each of which is a list of conditional schedule sets
    */
-  public List getConditionalScheduleSets () {
-    return conditionalScheduleSets;
+  public List getScheduleAlternatives () {
+    return scheduleAlternatives;
   }
  
-  /**
-   * Adds a conditional schedule set to the list which is evaluated in order by the
-   * job assigner to find the schedule set to assign.
+  /** Adds athe schedule alternaives, each of which is a list of conditional schedule sets.
    *
-   * @param conditionalScheduleSet the given conditional schedule set to add
+   * @param conditionalScheduleSets the given alternative conditional schedule sets to add
    */
-  public void addConditionalScheduleSet(ConditionalScheduleSet conditionalScheduleSet) {
-    conditionalScheduleSets.add(conditionalScheduleSet);
+  public void addScheduleAlternatives(List conditionalScheduleSets) {
+    scheduleAlternatives.add(conditionalScheduleSets);
   }
   
   //// Protected Area
 
-  /**
-   * Sets the schedule set condition list that associates a predicate expression with the set
-   * of schedules to carry out.
+  /** Sets the schedule alternaives, each of which is a list of conditional schedule sets.
    *
-   * @param conditionalScheduleSets the schedule set condition list that associates a predicate expression with the set
-   * of schedules to carry out
+   * @param scheduleAlternatives the schedule alternaives, each of which is a list of conditional schedule sets
    */
-  protected void setConditionalScheduleSets (List conditionalScheduleSets) {
-    this.conditionalScheduleSets = conditionalScheduleSets;
+  protected void setScheduleAlternatives (List scheduleAlternatives) {
+    this.scheduleAlternatives = scheduleAlternatives;
   }
   
   //// Private Area
@@ -356,11 +276,6 @@ public class TaskFrame {
   /** the constraints upon the performance of the task */
   protected List taskConstraints = new ArrayList();
 
-  /** 
-   * The schedule set condition list associates a predicate expression with the set
-   * of schedules to carry out.  The job  assigner iterates over this list and takes action 
-   * on the first element whose predicate expression evaluates to true.
-   */
-  protected List conditionalScheduleSets = new ArrayList();
-
+  /** the schedule alternaives, each of which is a list of conditional schedule sets */
+  protected List scheduleAlternatives = new ArrayList();
 }

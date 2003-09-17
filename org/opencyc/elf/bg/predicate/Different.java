9a12de6290992aad1a9dcbeb8917e5edd2987502
stephenreed <>
2003-09-17 13:30:53 +0000

    initial check in

---
+++ org/opencyc/elf/bg/predicate/Different.java
@@ -0,0 +1,90 @@
+package org.opencyc.elf.bg.predicate;
+
+//// Internal Imports
+import org.opencyc.elf.bg.state.State;
+
+//// External Imports
+import java.util.List;
+
+/**
+ * Different is a predicate of arity two that returns true if its arguments
+ * are not equal to each other.
+ *
+ * @version $Id$
+ * @author  reed
+ *
+ * <p>Copyright 2001 Cycorp, Inc., license is open source GNU LGPL.
+ * <p><a href="http://www.opencyc.org/license.txt">the license</a>
+ * <p><a href="http://www.opencyc.org">www.opencyc.org</a>
+ * <p><a href="http://www.sourceforge.net/projects/opencyc">OpenCyc at SourceForge</a>
+ * <p>
+ * THIS SOFTWARE AND KNOWLEDGE BASE CONTENT ARE PROVIDED ``AS IS'' AND
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OPENCYC
+ * ORGANIZATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE AND KNOWLEDGE
+ * BASE CONTENT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+public class Different extends Predicate {
+  
+  //// Constructors
+  
+  /** Creates a new instance of Different */
+  public Different() {
+    super();
+  }
+  
+  //// Public Area
+    
+  /** 
+   * Evaluates the given arguments and returns true if the first and second are
+   * not equal to each other.
+   *
+   * @param arguments the given arguments to evaluate
+   * @param state the given state
+   * @return true if the first and second are not equal to each other
+   */
+   public boolean evaluate (List arguments, State state) {
+     Object argument1 = evaluateArgument(arguments.get(0), state);
+     Object argument2 = evaluateArgument(arguments.get(1), state);
+     return ! argument1.equals(argument2);
+  }
+  
+  /**
+   * Returns a string representation of this predicate given
+   * the arguments.
+   *
+   * @param arguments the given arguments to evaluate
+   * @return a string representation of this object
+   */
+  public String toString(List arguments) {
+    StringBuffer stringBuffer = new StringBuffer();
+    stringBuffer.append("(different ");
+    stringBuffer.append(arguments.get(0).toString());
+    stringBuffer.append(")");
+    return stringBuffer.toString();
+  }
+  
+ /**
+   * Returns true if the given object equals this object.
+   *
+   * @param obj the given object
+   * @return true if the given object equals this object
+   */
+  public boolean equals(Object obj) {
+    return obj instanceof Different;
+  }
+  
+  //// Protected Area
+  
+  //// Private Area
+  
+  //// Internal Rep
+  
+}

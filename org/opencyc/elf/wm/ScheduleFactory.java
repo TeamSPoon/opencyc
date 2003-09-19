d172fa9348ad499921767d481894cfe78d6375e1
stephenreed <>
2003-09-19 22:24:19 +0000

    initial check in

---
+++ org/opencyc/elf/wm/ScheduleFactory.java
@@ -0,0 +1,43 @@
+/* $Id$
+ *
+ * Copyright (c) 2003 Cycorp, Inc.  All rights reserved.
+ * This software is the proprietary information of Cycorp, Inc.
+ * Use is subject to license terms.
+ */
+
+package org.opencyc.elf.wm;
+
+//// Internal Imports
+
+//// External Imports
+
+/**
+ * <P>ScheduleFactory is designed to...
+ *
+ * <P>Copyright (c) 2003 Cycorp, Inc.  All rights reserved.
+ * <BR>This software is the proprietary information of Cycorp, Inc.
+ * <P>Use is subject to license terms.
+ *
+ * @author reed
+ * @date September 19, 2003, 3:50 PM
+ * @version $Id$
+ */
+public class ScheduleFactory {
+  
+  //// Constructors
+  
+  /** Creates a new instance of ScheduleFactory. */
+  public ScheduleFactory() {
+  }
+  
+  //// Public Area
+  
+  //// Protected Area
+  
+  //// Private Area
+  
+  //// Internal Rep
+  
+  //// Main
+  
+}
---
+++ org/opencyc/elf/wm/ScheduleLibrary.java
@@ -0,0 +1,43 @@
+/* $Id$
+ *
+ * Copyright (c) 2003 Cycorp, Inc.  All rights reserved.
+ * This software is the proprietary information of Cycorp, Inc.
+ * Use is subject to license terms.
+ */
+
+package org.opencyc.elf.wm;
+
+//// Internal Imports
+
+//// External Imports
+
+/**
+ * <P>ScheduleLibrary is designed to...
+ *
+ * <P>Copyright (c) 2003 Cycorp, Inc.  All rights reserved.
+ * <BR>This software is the proprietary information of Cycorp, Inc.
+ * <P>Use is subject to license terms.
+ *
+ * @author reed
+ * @date September 19, 2003, 3:50 PM
+ * @version $Id$
+ */
+public class ScheduleLibrary {
+  
+  //// Constructors
+  
+  /** Creates a new instance of ScheduleLibrary. */
+  public ScheduleLibrary() {
+  }
+  
+  //// Public Area
+  
+  //// Protected Area
+  
+  //// Private Area
+  
+  //// Internal Rep
+  
+  //// Main
+  
+}

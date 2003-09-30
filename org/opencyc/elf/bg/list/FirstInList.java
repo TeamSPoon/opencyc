cd8ad0b8613a3278a438ca57b48f933dd6366786
stephenreed <>
2003-09-30 18:45:51 +0000

    initial check in

---
+++ org/opencyc/elf/bg/list/FirstInList.java
@@ -0,0 +1,77 @@
+package org.opencyc.elf.bg.list;
+
+//// Internal Imports
+import org.opencyc.elf.bg.expression.Operator;
+
+import org.opencyc.elf.wm.state.State;
+
+//// External Imports
+import java.util.List;
+
+/** FirstInList is an arity one operator that returns the first element of its list argument.
+ *
+ * @version $Id$
+ * @author  reed
+ *
+ * <p>Copyright 2001 Cycorp, Inc., license is open source GNU LGPL.
+ * <p><a href="http://www.opencyc.org/license.txt">the license</a>
+ * <p><a href="http://www.opencyc.org">www.opencyc.org</a>
+ * <p><a href="http://www.sourceforge.net/projects/opencyc">OpenCyc at SourceForge</a>
+ * <p>
+ * THIS SOFTWARE AND KNOWLEDGE BASE CONTENT ARE PROVIDED ``AS IS'' AND
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OPENCYC
+ * ORGANIZATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE AND KNOWLEDGE
+ * BASE CONTENT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+public class FirstInList extends Operator {
+  
+  //// Constructors
+  
+  /** Creates a new instance of FirstInList. */
+  public FirstInList() {
+    super();
+  }
+  
+  /** Evaluates the given argument within the given state and returns the first element of the list.
+   *
+   * @param arguments the given arguments to evaluate
+   * @param state the given state
+   */
+  public Object evaluate(List arguments, State state) {
+    List list = (List) evaluateArgument(arguments.get(0), state);
+    return list.get(0);
+  }
+  
+  /** Returns a string representation of this operator given
+   * the arguments.
+   *
+   * @param arguments the given arguments to evaluate
+   * @return a string representation of this object
+   */
+  public String toString(List arguments) {
+    StringBuffer stringBuffer = new StringBuffer();
+    stringBuffer.append("(first-in-list ");
+    stringBuffer.append(arguments.get(0).toString());
+    stringBuffer.append(")");
+    return stringBuffer.toString();
+  }
+  
+  //// Public Area
+  
+  //// Protected Area
+  
+  //// Private Area
+  
+  //// Internal Rep
+  
+  //// Main
+  
+}

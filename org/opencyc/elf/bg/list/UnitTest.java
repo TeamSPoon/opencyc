5fbccd266088c8a9b94f4f759fde3d6584bb00c2
stephenreed <>
2003-10-01 20:07:15 +0000

    added CycAccess

--- org/opencyc/elf/bg/list/UnitTest.java
+++ org/opencyc/elf/bg/list/UnitTest.java
@@ -1,6 +1,8 @@
 package org.opencyc.elf.bg.list;
 
 //// Internal Imports
+import org.opencyc.api.CycAccess;
+
 import org.opencyc.cycobject.CycList;
 
 import org.opencyc.elf.bg.expression.Operator;
@@ -69,104 +71,111 @@ public class UnitTest extends TestCase {
   /** Tests operator expression behavior. */
   public void testOperatorExpression() {
     System.out.println("\n*** testOperatorExpression ***");
-    State state = new State();
-    
-    // TheEmptyList
-    new TheEmptyList();
-    Operator theEmptyList = TheEmptyList.getInstance();
-    Assert.assertNotNull(theEmptyList.evaluate(null, state));
-    Assert.assertEquals(new CycList(), theEmptyList.evaluate(null, state));
-    OperatorExpression operatorExpression1 = new OperatorExpression(theEmptyList);
-    Assert.assertEquals("(the-empty-list)", operatorExpression1.toString());
-    Assert.assertEquals(new CycList(), operatorExpression1.evaluate(state));
-    
-    // TheList
-    new TheList();
-    Operator theList = TheList.getInstance();
-    operatorExpression1 = new OperatorExpression(theList, "xyz");
-    Assert.assertTrue(operatorExpression1.evaluate(state) instanceof List);
-    Assert.assertEquals(1, ((List) operatorExpression1.evaluate(state)).size());
-    List list = new CycList();
-    list.add("xyz");
-    Assert.assertEquals(list, operatorExpression1.evaluate(state));
-    operatorExpression1 = new OperatorExpression(theList, "xyz", new Integer(0), new Float(1.0));
-    Assert.assertEquals("(the-list \"xyz\" 0 1.0)", operatorExpression1.toString());
-    Assert.assertEquals(3, ((List) operatorExpression1.evaluate(state)).size());
-    
-    // LengthOfList
-    new LengthOfList();
-    Operator lengthOfList = LengthOfList.getInstance();
-    operatorExpression1 = new OperatorExpression(lengthOfList, new CycList());
-    Assert.assertEquals(new Integer(0), operatorExpression1.evaluate(state));
-    list = new CycList();
-    list.add("abc");
-    list.add(new Double(100.0d));
-    operatorExpression1 = new OperatorExpression(lengthOfList, list);
-    Assert.assertEquals(new Integer(2), operatorExpression1.evaluate(state));
-    StateVariable testStateVariable1 = new StateVariable(List.class, "test-state-variable1", "test state variable 1");
-    operatorExpression1 = new OperatorExpression(lengthOfList, testStateVariable1);
-    Assert.assertEquals("(length-of-list test-state-variable1)", operatorExpression1.toString());
-    state.setStateValue(testStateVariable1, new CycList());
-    Assert.assertEquals(new Integer(0), operatorExpression1.evaluate(state));
-    state.setStateValue(testStateVariable1, list);
-    Assert.assertEquals(new Integer(2), operatorExpression1.evaluate(state));
-    
-    // JoinLists
-    new JoinLists();
-    Operator joinLists = JoinLists.getInstance();
-    operatorExpression1 = new OperatorExpression(joinLists, new CycList(), new CycList());
-    Assert.assertEquals(new CycList(), operatorExpression1.evaluate(state));
-    List list1 = new CycList();
-    list1.add("abc");
-    list1.add(new Double(100.0d));
-    operatorExpression1 = new OperatorExpression(joinLists, list1, new CycList());
-    Assert.assertEquals(list1, operatorExpression1.evaluate(state));
-    Assert.assertEquals("(join-lists (\"abc\" 100.0) ())", operatorExpression1.toString());
-    operatorExpression1 = new OperatorExpression(joinLists, new CycList(), list1);
-    Assert.assertEquals(list1, operatorExpression1.evaluate(state));
-    List list2 = new CycList();
-    list2.add(new Integer(0));
-    list2.add("def");
-    operatorExpression1 = new OperatorExpression(joinLists, list1, list2);
-    Assert.assertTrue(operatorExpression1.evaluate(state) instanceof List);
-    Assert.assertTrue(((List) operatorExpression1.evaluate(state)).size() == 4);
-    Assert.assertTrue(((List) operatorExpression1.evaluate(state)).contains("abc"));
-    Assert.assertTrue(((List) operatorExpression1.evaluate(state)).contains("def"));
-    operatorExpression1 = new OperatorExpression(joinLists, list1, testStateVariable1);
-    state.setStateValue(testStateVariable1, list1);
-    Assert.assertTrue(((List) operatorExpression1.evaluate(state)).size() == 4);
-    Assert.assertTrue(((List) operatorExpression1.evaluate(state)).contains("abc"));
-    Assert.assertTrue(! ((List) operatorExpression1.evaluate(state)).contains("def"));
-    
-    // FirstInList
-    new FirstInList();
-    Operator firstInList = FirstInList.getInstance();
-    operatorExpression1 = new OperatorExpression(firstInList, list1);
-    Assert.assertEquals("abc", operatorExpression1.evaluate(state));
-    state.setStateValue(testStateVariable1, list2);
-    operatorExpression1 = new OperatorExpression(firstInList, testStateVariable1);
-    Assert.assertEquals(new Integer(0), operatorExpression1.evaluate(state));
-    Assert.assertEquals("(first-in-list test-state-variable1)", operatorExpression1.toString());
-    
-    // RestOfList
-    new RestOfList();
-    Operator restOfList = RestOfList.getInstance();
-    operatorExpression1 = new OperatorExpression(restOfList, list1);
-    List list3 = new CycList();
-    list3.add(new Double(100.0d));
-    Assert.assertEquals(list3, operatorExpression1.evaluate(state));
-    Assert.assertEquals("(rest-of-list (\"abc\" 100.0))", operatorExpression1.toString());
-    
-    // (join-lists (the-list (first-in-list ("abc" 100.0))) (rest-of-list ("abc" 100.0)))
-    operatorExpression1 = new OperatorExpression(firstInList, list1);
-    OperatorExpression operatorExpression2 = new OperatorExpression(restOfList, list1);
-    OperatorExpression operatorExpression3 = 
-      new OperatorExpression(theList, operatorExpression1);
-    OperatorExpression operatorExpression4 = 
-      new OperatorExpression(joinLists, operatorExpression3, operatorExpression2);
-    Assert.assertEquals("(join-lists (the-list (first-in-list (\"abc\" 100.0))) (rest-of-list (\"abc\" 100.0)))", 
-                        operatorExpression4.toString());
-    Assert.assertEquals(list1, operatorExpression4.evaluate(state));
+    try {
+      new CycAccess();
+      State state = new State();
+
+      // TheEmptyList
+      new TheEmptyList();
+      Operator theEmptyList = TheEmptyList.getInstance();
+      Assert.assertNotNull(theEmptyList.evaluate(null, state));
+      Assert.assertEquals(new CycList(), theEmptyList.evaluate(null, state));
+      OperatorExpression operatorExpression1 = new OperatorExpression(theEmptyList);
+      Assert.assertEquals("(the-empty-list)", operatorExpression1.toString());
+      Assert.assertEquals(new CycList(), operatorExpression1.evaluate(state));
+
+      // TheList
+      new TheList();
+      Operator theList = TheList.getInstance();
+      operatorExpression1 = new OperatorExpression(theList, "xyz");
+      Assert.assertTrue(operatorExpression1.evaluate(state) instanceof List);
+      Assert.assertEquals(1, ((List) operatorExpression1.evaluate(state)).size());
+      List list = new CycList();
+      list.add("xyz");
+      Assert.assertEquals(list, operatorExpression1.evaluate(state));
+      operatorExpression1 = new OperatorExpression(theList, "xyz", new Integer(0), new Float(1.0));
+      Assert.assertEquals("(the-list \"xyz\" 0 1.0)", operatorExpression1.toString());
+      Assert.assertEquals(3, ((List) operatorExpression1.evaluate(state)).size());
+
+      // LengthOfList
+      new LengthOfList();
+      Operator lengthOfList = LengthOfList.getInstance();
+      operatorExpression1 = new OperatorExpression(lengthOfList, new CycList());
+      Assert.assertEquals(new Integer(0), operatorExpression1.evaluate(state));
+      list = new CycList();
+      list.add("abc");
+      list.add(new Double(100.0d));
+      operatorExpression1 = new OperatorExpression(lengthOfList, list);
+      Assert.assertEquals(new Integer(2), operatorExpression1.evaluate(state));
+      StateVariable testStateVariable1 = new StateVariable(List.class, "test-state-variable1", "test state variable 1");
+      operatorExpression1 = new OperatorExpression(lengthOfList, testStateVariable1);
+      Assert.assertEquals("(length-of-list test-state-variable1)", operatorExpression1.toString());
+      state.setStateValue(testStateVariable1, new CycList());
+      Assert.assertEquals(new Integer(0), operatorExpression1.evaluate(state));
+      state.setStateValue(testStateVariable1, list);
+      Assert.assertEquals(new Integer(2), operatorExpression1.evaluate(state));
+
+      // JoinLists
+      new JoinLists();
+      Operator joinLists = JoinLists.getInstance();
+      operatorExpression1 = new OperatorExpression(joinLists, new CycList(), new CycList());
+      Assert.assertEquals(new CycList(), operatorExpression1.evaluate(state));
+      List list1 = new CycList();
+      list1.add("abc");
+      list1.add(new Double(100.0d));
+      operatorExpression1 = new OperatorExpression(joinLists, list1, new CycList());
+      Assert.assertEquals(list1, operatorExpression1.evaluate(state));
+      Assert.assertEquals("(join-lists (\"abc\" 100.0) ())", operatorExpression1.toString());
+      operatorExpression1 = new OperatorExpression(joinLists, new CycList(), list1);
+      Assert.assertEquals(list1, operatorExpression1.evaluate(state));
+      List list2 = new CycList();
+      list2.add(new Integer(0));
+      list2.add("def");
+      operatorExpression1 = new OperatorExpression(joinLists, list1, list2);
+      Assert.assertTrue(operatorExpression1.evaluate(state) instanceof List);
+      Assert.assertTrue(((List) operatorExpression1.evaluate(state)).size() == 4);
+      Assert.assertTrue(((List) operatorExpression1.evaluate(state)).contains("abc"));
+      Assert.assertTrue(((List) operatorExpression1.evaluate(state)).contains("def"));
+      operatorExpression1 = new OperatorExpression(joinLists, list1, testStateVariable1);
+      state.setStateValue(testStateVariable1, list1);
+      Assert.assertTrue(((List) operatorExpression1.evaluate(state)).size() == 4);
+      Assert.assertTrue(((List) operatorExpression1.evaluate(state)).contains("abc"));
+      Assert.assertTrue(! ((List) operatorExpression1.evaluate(state)).contains("def"));
+
+      // FirstInList
+      new FirstInList();
+      Operator firstInList = FirstInList.getInstance();
+      operatorExpression1 = new OperatorExpression(firstInList, list1);
+      Assert.assertEquals("abc", operatorExpression1.evaluate(state));
+      state.setStateValue(testStateVariable1, list2);
+      operatorExpression1 = new OperatorExpression(firstInList, testStateVariable1);
+      Assert.assertEquals(new Integer(0), operatorExpression1.evaluate(state));
+      Assert.assertEquals("(first-in-list test-state-variable1)", operatorExpression1.toString());
+
+      // RestOfList
+      new RestOfList();
+      Operator restOfList = RestOfList.getInstance();
+      operatorExpression1 = new OperatorExpression(restOfList, list1);
+      List list3 = new CycList();
+      list3.add(new Double(100.0d));
+      Assert.assertEquals(list3, operatorExpression1.evaluate(state));
+      Assert.assertEquals("(rest-of-list (\"abc\" 100.0))", operatorExpression1.toString());
+
+      // (join-lists (the-list (first-in-list ("abc" 100.0))) (rest-of-list ("abc" 100.0)))
+      operatorExpression1 = new OperatorExpression(firstInList, list1);
+      OperatorExpression operatorExpression2 = new OperatorExpression(restOfList, list1);
+      OperatorExpression operatorExpression3 = 
+        new OperatorExpression(theList, operatorExpression1);
+      OperatorExpression operatorExpression4 = 
+        new OperatorExpression(joinLists, operatorExpression3, operatorExpression2);
+      Assert.assertEquals("(join-lists (the-list (first-in-list (\"abc\" 100.0))) (rest-of-list (\"abc\" 100.0)))", 
+                          operatorExpression4.toString());
+      Assert.assertEquals(list1, operatorExpression4.evaluate(state));
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      Assert.fail(e.getMessage());
+    }
     
     System.out.println("*** testOperatorExpression OK ***");
   }

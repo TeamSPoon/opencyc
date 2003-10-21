55ba768129675b531c3880c56af55004c9c281b1
stephenreed <>
2003-10-21 23:32:19 +0000

    initial check in

---
+++ org/opencyc/elf/bg/command/GoalCommand.java
@@ -0,0 +1,43 @@
+/* $Id$
+ *
+ * Copyright (c) 2003 Cycorp, Inc.  All rights reserved.
+ * This software is the proprietary information of Cycorp, Inc.
+ * Use is subject to license terms.
+ */
+
+package org.opencyc.elf.bg.command;
+
+//// Internal Imports
+
+//// External Imports
+
+/**
+ * <P>GoalCommand is designed to...
+ *
+ * <P>Copyright (c) 2003 Cycorp, Inc.  All rights reserved.
+ * <BR>This software is the proprietary information of Cycorp, Inc.
+ * <P>Use is subject to license terms.
+ *
+ * @author reed
+ * @date October 21, 2003, 5:30 PM
+ * @version $Id$
+ */
+public class GoalCommand {
+  
+  //// Constructors
+  
+  /** Creates a new instance of GoalCommand. */
+  public GoalCommand() {
+  }
+  
+  //// Public Area
+  
+  //// Protected Area
+  
+  //// Private Area
+  
+  //// Internal Rep
+  
+  //// Main
+  
+}
